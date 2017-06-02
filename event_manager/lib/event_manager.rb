2c8769a35cf37867efd60d1cafee61da049f3c6f
Kamila Matla-Tomczyk <ka1130@o2.pl>
2017-06-02 12:30:01 +0200

    Rewrite code to use csv parser

--- event_manager/lib/event_manager.rb
+++ event_manager/lib/event_manager.rb
@@ -1,11 +1,10 @@
+require "csv"
 puts "EventManager Initialized!"
 
-File.exist? "event_attendees.csv"
+# File.exist? "event_attendees.csv"
 
-lines = File.readlines "event_attendees.csv"
-lines.each do |line|
-  next if line == " ,RegDate,first_Name,last_Name,Email_Address,HomePhone,Street,City,State,Zipcode\n"
-  columns = line.split(",")
-  name = columns[2]
+contents = CSV.open "event_attendees.csv", headers: true
+contents.each do |row|
+  name = row[2]
   puts name
 end
