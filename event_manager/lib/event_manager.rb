c927120cb5d77e9317f9b283b0026ce173ba6c38
Kamila Matla-Tomczyk <ka1130@o2.pl>
2017-06-02 14:00:59 +0200

    Add legislators

--- event_manager/lib/event_manager.rb
+++ event_manager/lib/event_manager.rb
@@ -1,4 +1,7 @@
 require 'csv'
+require 'sunlight/congress'
+
+Sunlight::Congress.api_key = "e179a6973728c4dd3fb1204283aaccb5"
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5,"0")[0..4]
@@ -11,6 +14,7 @@ contents = CSV.open 'event_attendees.csv', headers: true, header_converters: :sy
 contents.each do |row|
   name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
+  legislators = Sunlight::Congress::Legislator.by_zipcode(zipcode)
 
-  puts "#{name} #{zipcode}"
+  puts "#{name} #{zipcode} #{legislators}"
 end
