da599efad05c2c619e636f117e7d01f97afb8122
Kamila Matla-Tomczyk <ka1130@o2.pl>
2017-06-02 12:34:08 +0200

    Add zipcode as symbol

--- event_manager/lib/event_manager.rb
+++ event_manager/lib/event_manager.rb
@@ -3,8 +3,9 @@ puts "EventManager Initialized!"
 
 # File.exist? "event_attendees.csv"
 
-contents = CSV.open "event_attendees.csv", headers: true
+contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
 contents.each do |row|
-  name = row[2]
-  puts name
+  name = row[:first_name]
+  zipcode = row[:zipcode]
+  puts "#{name} #{zipcode}"
 end
