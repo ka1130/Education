34a7e32ea3e321048e8fd92851d831d1418d7751
Kamila Matla-Tomczyk <ka1130@o2.pl>
2017-06-02 12:13:04 +0200

    Read the file and split the columns

--- event_manager/lib/event_manager.rb
+++ event_manager/lib/event_manager.rb
@@ -1,1 +1,9 @@
 puts "EventManager Initialized!"
+
+File.exist? "event_attendees.csv"
+
+lines = File.readlines "event_attendees.csv"
+lines.each do |line|
+  columns = line.split(",")
+  puts columns
+end
