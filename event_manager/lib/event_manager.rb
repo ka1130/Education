90ba6c92fbd8cd363170f734a0e8ebb553b892bb
Kamila Matla-Tomczyk <ka1130@o2.pl>
2017-06-02 12:21:15 +0200

    Print column with first names only

--- event_manager/lib/event_manager.rb
+++ event_manager/lib/event_manager.rb
@@ -4,6 +4,8 @@ File.exist? "event_attendees.csv"
 
 lines = File.readlines "event_attendees.csv"
 lines.each do |line|
+  next if line == " ,RegDate,first_Name,last_Name,Email_Address,HomePhone,Street,City,State,Zipcode\n"
   columns = line.split(",")
-  puts columns
+  name = columns[2]
+  puts name
 end
