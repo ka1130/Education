f1935de63ae082d030979fdd21b631ee7afdc460
Kamila Matla-Tomczyk <ka1130@o2.pl>
2017-06-02 10:22:32 +0200

    Fix name and initialize methods

---
+++ unicorn/unicorn.rb
@@ -0,0 +1,9 @@
+class Unicorn
+  def initialize(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+end
--- unicorn/unicorn_test.rb
+++ unicorn/unicorn_test.rb
@@ -8,6 +8,11 @@ class UnicornTest < Minitest::Test
     unicorn = Unicorn.new("Robert")
     assert_equal "Robert", unicorn.name
   end
+
+  def test_it_can_be_named_something_else
+    unicorn = Unicorn.new("Joseph")
+    assert_equal "Joseph", unicorn.name
+  end
 end
 
 
