20a60028ef31efb974996c3a5709557203b85bf9
Kamila Matla-Tomczyk <ka1130@o2.pl>
2017-06-12 15:48:24 +0200

    First lesson complete, 100% passed

--- algorithms/ex_1.rb
+++ algorithms/ex_1.rb
@@ -1,15 +1,33 @@
 def solution(n)
- binary_arr = n.to_s(2).split("")
+ 
   counter = 0
+  max_value = 0
 
-  binary_arr.each_with_index do |element, index| 
+  if n == 1
+    return counter
+  else 
 
-    if binary_arr[index] == "0"
-      counter = counter + 1
+    binary_arr = n.to_s(2).split("")
+    
+    binary_arr.each do |number| 
+    
+      if number == "1"
+
+        if counter > max_value
+          max_value = counter
+        end
+
+        counter = 0
+        
+      elsif number == "0"
+        counter += 1 
+        next            
+      end
+     
     end
-
-    if binary_arr[index + 1] == "1" 
-      return counter
-    end
+  
+   return max_value
+    
   end
+
 end
--- algorithms/ex_1_test.rb
+++ algorithms/ex_1_test.rb
@@ -5,8 +5,36 @@ require_relative 'ex_1'
 
 class AlgorithmTest < Minitest::Test
 
-  def test_ex1
-    assert_equal 5, solution(1041)
+  def test_ex1_01
+    assert_equal 0, solution(1) 
+  end
+
+  def test_ex1_02
+    assert_equal 5, solution(1_041)
+  end
+
+  def test_ex1_03
+    assert_equal 0, solution(2_147_483_647)
+  end
+
+  def test_ex1_04
+    assert_equal 0, solution(4)
+  end
+
+  def test_ex1_05
+    assert_equal 0, solution(16)
+  end
+
+  def test_ex1_06
+    assert_equal 0, solution(15)
+  end
+
+  def test_ex1_07
+    assert_equal 2, solution(328)
+  end
+
+  def test_ex1_08
+    assert_equal 0, solution(6)
   end
 
 end
