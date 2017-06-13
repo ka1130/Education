b259e9042e150ac22545d29538b6cce0da6fe713
Kamila Matla-Tomczyk <ka1130@o2.pl>
2017-06-13 15:17:46 +0200

    Ex2, arrays, 100%

--- algorithms/Lesson2_Arrays/ex_2_test.rb
+++ algorithms/Lesson2_Arrays/ex_2_test.rb
@@ -11,4 +11,12 @@ class AlgorithmTest < Minitest::Test
   def test_2
     assert_equal [21, 7, -1000, 3, 999], solution([-1000, 3, 999, 21, 7], 2)
   end
+
+  def test_times_bigger_than_array_length
+    assert_equal [5, 3, 3, 3, 5], solution([3, 3, 5, 5, 3], 42)
+  end
+
+  def test_empty_array
+    assert_equal [], solution([], 1)
+  end
 end
