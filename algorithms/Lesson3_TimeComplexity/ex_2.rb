706b5fde3155dd9224e4a37b4bfea537777c0782
Kamila Matla-Tomczyk <ka1130@o2.pl>
2017-06-14 11:09:56 +0200

    Correct formatting

---
+++ algorithms/Lesson3_TimeComplexity/ex_2.rb
@@ -0,0 +1,1 @@
+def solution(array); end
--- algorithms/Lesson3_TimeComplexity/ex_2_test.rb
+++ algorithms/Lesson3_TimeComplexity/ex_2_test.rb
@@ -1,14 +1,10 @@
 gem 'minitest', '~> 5.0'
 require 'minitest/autorun'
 require 'minitest/pride'
-require_relative 'ex_1'
+require_relative 'ex_2'
 
 class AlgorithmTest < Minitest::Test
   def test_basic
     assert_equal 5, solution([1, 8, 4, 7, 3, 2, 6])
-  end
-
-  def test_empty_array
-    assert_equal 1, solution([])
   end
 end
