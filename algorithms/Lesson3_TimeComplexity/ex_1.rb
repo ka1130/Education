b252fd4240e3247345d8745d868c37b56378f413
Kamila Matla-Tomczyk <ka1130@o2.pl>
2017-06-14 11:06:37 +0200

    Improve function, get rid of unnecessary reduce method

--- algorithms/Lesson3_TimeComplexity/ex_1.rb
+++ algorithms/Lesson3_TimeComplexity/ex_1.rb
@@ -1,14 +1,8 @@
 def solution(array)
   return 1 if array == []
-  current_sum = array.reduce(:+)
-  correct_array = []
   n = array.length + 1
-  counter = 0
-  n.times do
-    counter += 1
-    correct_array.push(counter)
-  end
-  correct_sum = correct_array.reduce(:+)
+  current_sum = array.reduce(:+)
+  correct_sum = ((1.0 + n) / 2.0) * n
   missing_number = correct_sum - current_sum
-  missing_number
+  missing_number.to_i
 end
