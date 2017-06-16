22af6cbd73440a2011ee4d6debec1675802e0f71
Kamila Matla-Tomczyk <ka1130@o2.pl>
2017-06-16 10:35:09 +0200

    3 exercise done, 100%

--- algorithms/Lesson3_TimeComplexity/ex_3.rb
+++ algorithms/Lesson3_TimeComplexity/ex_3.rb
@@ -1,44 +1,14 @@
 def solution(array)
   n = array.length - 1
   counter = 0
-  results = []
-  sums = []
-  arrays = []
-  n.times do
-    first_part = array[0..counter]
-    second_part = array[(counter + 1)..n]
-    arrays.push(first_part)
-    arrays.push(second_part)
+  left_sum = 0
+  right_sum = array.reduce(0, :+)
+  result = Float::INFINITY
+  while counter < n
+    left_sum += array[counter]
+    right_sum -= array[counter]
+    result = (left_sum - right_sum).abs if (left_sum - right_sum).abs < result
     counter += 1
   end
-  m = arrays.length - 1
-  for i in 0..m do
-    sums.push(arrays[i].reduce(0, :+))
-  end
-  o = sums.length / 2
-  i = 0
-  o.times do
-    results.push(sums[i].to_i - sums[i + 1].to_i)
-    i += 2
-  end
-  results.map(&:abs).min
+  result
 end
-
-# def count_sum(array)
-#   array.reduce(0, :+)
-# end
-
-# def solution(array)
-#   n = array.length - 1
-#   counter = 0
-#   results = []
-#   n.times do
-#     first_part = array[0..counter]
-#     second_part = array[(counter + 1)..n]
-#     first_sum = count_sum(first_part)
-#     second_sum = count_sum(second_part)
-#     results.push(first_sum - second_sum)
-#     counter += 1
-#   end
-#   results.map(&:abs).min
-# end
