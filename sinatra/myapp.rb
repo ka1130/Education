64b39bf1666c359ed2eb1db8476537808cea552a
Kamila Matla-Tomczyk <ka1130@o2.pl>
2017-06-30 12:35:39 +0200

    Add basic form

--- sinatra/myapp.rb
+++ sinatra/myapp.rb
@@ -4,6 +4,7 @@ get '/' do
   output = 'Hello world!'
   output << '<br />'
   output << '<a href=\'/goodbye\'>Leave</a>'
+  output
 end
 
 get '/goodbye' do
@@ -14,4 +15,15 @@ get '/about' do
   output = '<h2>Sinatra</h2>'
   output << '<p>This is my first Sinatra app</p>'
   output
+end
+
+get '/form' do
+  form = '<form action="/form" method="post">'
+  form << '<input type="submit" value="submit" />'
+  form << '</form>'
+  form
+end
+
+post '/form' do
+  'Thank you for submitting the form.'
 end
