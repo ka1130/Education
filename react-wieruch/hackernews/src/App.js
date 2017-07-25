9feb883f03e31736f77ba63506435027fc538d05
Kamila Matla-Tomczyk <ka1130@o2.pl>
2017-07-25 14:05:43 +0200

    Add next element.

--- react-wieruch/hackernews/src/App.js
+++ react-wieruch/hackernews/src/App.js
@@ -3,9 +3,12 @@ import './App.css';
 
 class App extends Component {
   render() {
+    var helloWorld = 'Welcome to the Road to learn React';
+    var paragraph = 'This is the first paragraph';
     return (
       <div className="App">
-        <h2>Welcome to React</h2>
+        <h2>{helloWorld}</h2>
+        <p className="Paragraph">{paragraph}</p>
       </div>
     );
   }
