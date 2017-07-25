27a629a6843be756ac204fa19152732ce00eecaa
Kamila Matla-Tomczyk <ka1130@o2.pl>
2017-07-25 16:38:46 +0200

    Add more elements

--- react-wieruch/hackernews/src/App.js
+++ react-wieruch/hackernews/src/App.js
@@ -1,14 +1,42 @@
 import React, {Component} from 'react';
 import './App.css';
 
+function formatName(user) {
+  return 'Hello, ' + user.firstName + ' ' + user.lastName;
+}
+
+const user = {
+  firstName: 'John',
+  lastName: 'Smith'
+}
+
+const list = [
+  {
+    title: 'React',
+    url: 'https://facebook.github.io/react/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0
+  }, {
+    title: 'Redux',
+    url: 'https://github.com/reactjs/redux',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1
+  }
+];
+
 class App extends Component {
   render() {
-    var helloWorld = 'Welcome to the Road to learn React';
-    var paragraph = 'This is the first paragraph';
+    const helloWorld = 'Welcome to the Road to learn React';
+    const paragraph = 'This is the first paragraph';
     return (
       <div className="App">
         <h2>{helloWorld}</h2>
         <p className="Paragraph">{paragraph}</p>
+        <h5>{formatName(user)}</h5>
       </div>
     );
   }
