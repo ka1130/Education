a765728868cb3bcb23b76367fed407ea44b6fa4e
Kamila Matla-Tomczyk <ka1130@o2.pl>
2017-07-25 16:48:37 +0200

    Make use of more keys

--- react-wieruch/hackernews/src/App.js
+++ react-wieruch/hackernews/src/App.js
@@ -34,7 +34,14 @@ class App extends Component {
       <div className="App">
         {list
           .map(function (item) {
-            return <div>{item.title}</div>;
+            return <div>
+              <span>
+                <a href={item.url}>item.title</a>
+              </span>
+              <span>item.author</span>
+              <span>item.num_comments</span>
+              <span>item.points</span>
+            </div>;
           })}
       </div>
     );
