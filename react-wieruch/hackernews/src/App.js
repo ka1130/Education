c5ff5c97cb3bae9cd78904e4aa2f0058f3282797
Kamila Matla-Tomczyk <ka1130@o2.pl>
2017-07-27 16:21:33 +0200

    Format code

--- react-wieruch/hackernews/src/App.js
+++ react-wieruch/hackernews/src/App.js
@@ -41,8 +41,8 @@ class App extends Component {
   }
 
   onDismiss(id) {
-    const isNotId = item => item.objectID !== id;
-    const updatedList = this.state.list.filter(isNotId);
+    const updatedList = this.state.list.filter(item => item.objectID !== id);
+    this.setState({ list: updatedList });
   }
 
   render() {
