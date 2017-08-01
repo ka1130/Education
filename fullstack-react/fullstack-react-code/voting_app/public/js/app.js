216ee87bdf344cd7c46d9ffe1615d53707ccbe9d
Kamila Matla-Tomczyk <ka1130@o2.pl>
2017-08-01 14:13:25 +0200

    Sort products

--- fullstack-react/fullstack-react-code/voting_app/public/js/app.js
+++ fullstack-react/fullstack-react-code/voting_app/public/js/app.js
@@ -28,7 +28,10 @@ class Product extends React.Component {
 
 class ProductList extends React.Component {
   render() {
-    const productComponents = Seed.products.map((product) => (
+    const products = Seed.products.sort((a, b) => (
+      b.votes - a.votes
+    ));
+    const productComponents = products.map((product) => (
       <Product
         key={'product-' + product.id}
         id={product.id}
