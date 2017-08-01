8c30bafcc1d9c85fcd2fb21962baedb86e35c76f
Kamila Matla-Tomczyk <ka1130@o2.pl>
2017-08-01 12:36:34 +0200

    Render Product

--- fullstack-react/fullstack-react-code/voting_app/public/js/app.js
+++ fullstack-react/fullstack-react-code/voting_app/public/js/app.js
@@ -24,7 +24,7 @@ class ProductList extends React.Component {
   render() {
     return (
         <div className="ui unstackable items">
-          Hello, friend! I am a basic React component.
+          <Product />
         </div>
       );
   }
