4901962e36975cb4f25da909fd2d4eaf0abcb83d
Kamila Matla-Tomczyk <ka1130@o2.pl>
2017-08-02 12:52:33 +0200

    Add EditableTimer class

--- fullstack-react/fullstack-react-code/time_tracking_app/public/js/app.js
+++ fullstack-react/fullstack-react-code/time_tracking_app/public/js/app.js
@@ -12,3 +12,48 @@ class TimersDashboard extends React.Component {
       );
   }
 }
+
+class EditableTimerList extends React.Component {
+  render() {
+    return (
+        <div id="timers">
+          <EditableTimer
+            title="Learn React"
+            project="Web Domination"
+            elapsed="8986300"
+            runningSince={null}
+            editFormOpen={false}
+          />
+          <EditableTimer
+            title="Learn extreme ironing"
+            project="World Domination"
+            elapsed="3890985"
+            runningSince={null}
+            editFormOpen={true}
+          />
+        </div>
+      );
+  }
+}
+
+class EditableTimer extends React.Component {
+  render () {
+    if (this.props.editFormOpen) {
+      return (
+          <TimerForm
+            title={this.props.title}
+            project={this.props.project}
+          />
+        );
+    } else {
+      return (
+          <Timer
+            title={this.props.title}
+            project={this.props.project}
+            elapsed={this.props.elapsed}
+            runningSince={this.props.runningSince}
+          />
+        );
+    }
+  }
+}
