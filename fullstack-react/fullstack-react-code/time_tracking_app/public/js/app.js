c0eda1c9c4d6ebd453cb50d1e3f1c5f084d7fa85
Kamila Matla-Tomczyk <ka1130@o2.pl>
2017-08-03 11:01:40 +0200

    Rewrite TimersDashboard

--- fullstack-react/fullstack-react-code/time_tracking_app/public/js/app.js
+++ fullstack-react/fullstack-react-code/time_tracking_app/public/js/app.js
@@ -1,21 +1,35 @@
 class TimersDashboard extends React.Component {
   state = {
-  timers: [
-    {
-      title: 'Practice squat',
-      project: 'Gym Chores',
-      id: uuid.v4(),
-      elapsed: 5456099,
-      runningSince: Date.now(),
-    },
-    {
-      title: 'Bake squash',
-      project: 'Kitchen Chores',
-      id: uuid.v4(),
-      elapsed: 1273998,
-      runningSince: null,
-    },
-  ],
+    timers: [
+      {
+        title: 'Practice squat',
+        project: 'Gym Chores',
+        id: uuid.v4(),
+        elapsed: 5456099,
+        runningSince: Date.now(),
+      },
+      {
+        title: 'Bake squash',
+        project: 'Kitchen Chores',
+        id: uuid.v4(),
+        elapsed: 1273998,
+        runningSince: null,
+      },
+    ],
+  }
+
+  render() {
+    return (
+        <div className="ui three column centered grid">
+          <div className="column">
+            <EditableTimerList />
+            <ToggleableTimerForm
+              isOpen={true}
+            />
+          </div>
+        </div>
+      );
+  }
 }
 
 class EditableTimerList extends React.Component {
