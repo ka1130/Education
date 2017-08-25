8c929c60004cee8dce05922941ba4436dd635dbb
Kamila Matla-Tomczyk <ka1130@o2.pl>
2017-08-25 11:59:25 +0200

    Delet unnecessary code

--- react-exercises/gist-api/src/App.js
+++ react-exercises/gist-api/src/App.js
@@ -2,45 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 import FetchDemo from './components/FetchDemo';
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://facebook.github.io/react/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://github.com/reactjs/redux',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
-
 class App extends Component {
   constructor(props) {
     super(props);
-
-    this.state = {
-      list,
-    };
   }
+
   render() {
-    const helloWorld = "Hello World";
     return (
       <div className="App">
-        {this.state.list.map( item => <div key={item.objectID}>
-            <span>
-              <a href={item.url}>{item.title}</a>
-            </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-          </div>)}
           <FetchDemo subreddit="reactjs"/>
       </div>
     );
