2dc984525546104579fc380d27804c6f9901b576
Kamila Matla-Tomczyk <ka1130@o2.pl>
2017-08-25 11:58:03 +0200

    First request with axios

--- react-exercises/gist-api/src/components/FetchDemo.js
+++ react-exercises/gist-api/src/components/FetchDemo.js
@@ -1,16 +1,35 @@
 import React, { Component } from 'react';
-
+import ReactDOM from 'react-dom';
+import axios from 'axios';
 
 class FetchDemo extends Component {
   constructor(props) {
-    super(props)
+    super(props);
+
+    this.state = {
+      posts: [],
+    };
   }
+
+  componentDidMount() {
+    axios.get(`http://www.reddit.com/r/${this.props.subreddit}.json`)
+      .then(result => {
+        const posts = result.data.data.children.map(obj => obj.data);
+        this.setState({ posts });
+      });
+  }
+
   render() {
     return (
       <div>
-        <p>FetchDemo</p>
+        <h1>{`/r/${this.props.subreddit}`}</h1>
+        <ul>
+          {this.state.posts.map(post =>
+            <li key={post.id}>{post.title}</li>
+            )}
+        </ul>
       </div>
-      )
+      );
   }
 }
 
