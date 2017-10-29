69fb47dde6e6ad7e0c1c1311edd6b79a09151db9
Kamila Matla-Tomczyk <ka1130@o2.pl>
2017-10-29 09:58:57 +0100

    Add axios to the single gist component

--- react-exercises/gist-api/src/components/SingleGist.js
+++ react-exercises/gist-api/src/components/SingleGist.js
@@ -1,11 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-const SingleGist = props => {
-  return (
-    <div>
-      <p>Single Gist</p>
-    </div>
-  );
+class SingleGist extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      id: "",
+      content: "",
+      created: "",
+      desc: "",
+    }
+  }
+
+  componentDidMount() {
+    const id = 42;
+    axios.get(`https://private-anon-8502f36fff-awapp.apiary-mock.com/gists/${id}`)
+      .then(result => {
+        console.log(result);
+        this.setState({
+          id: result.data.id,
+          content: result.data.content,
+          created: result.data.created_at,
+          desc: result.data.description,
+        })
+      })
+      .catch(error => {
+        console.err(error);
+      });
+  }
+
+  render() {
+    const {id, content, created, dec} = this.state.id;
+
+    return (
+      <div className="description gist-description">
+        <h4 className="gist-header">{this.state.desc}</h4>
+        <h6 className="gist-date">Created at {this.state.created}</h6>
+        <p><strong>Gist No {this.state.id}</strong>: {this.state.content}</p>
+      </div>
+    );
+  }
+
 }
 
 export default SingleGist;
