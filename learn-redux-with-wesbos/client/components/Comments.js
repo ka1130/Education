38f65174e47b37020843764820f474e4f3a2df89
Kamila Matla-Tomczyk <ka1130@o2.pl>
2018-10-24 09:59:04 +0200

    Add a single Photo Comment

---
+++ learn-redux-with-wesbos/client/components/Comments.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Comments = (props) => {
+  return (
+    <div className="comment">
+      I'm the comment!
+    </div>
+  );
+}
+
+export default Comments;
--- learn-redux-with-wesbos/client/components/Single.js
+++ learn-redux-with-wesbos/client/components/Single.js
@@ -1,17 +1,17 @@
 import React from 'react';
+
 import Photo from './Photo';
+import Comments from './Comments';
 
-// import comments
-
-const Single = props => {
+const Single = (props) => {
   // we need the index of the post to get us the actual post
   // we have the post code/id in the url so we'll use that
-  const i = props.posts.findIndex(post => post.code === props.params.postId);
+  const i = props.posts.findIndex((post) => post.code === props.params.postId);
   const post = props.posts[i];
-  console.log(i, post);
   return (
     <div className="single-photo">
       <Photo i={i} post={post} {...props} />
+      <Comments />
     </div>
   );
 };
