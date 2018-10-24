1ed7428bd9604ac3b6a312ac1843c50c5eeaac8b
Kamila Matla-Tomczyk <ka1130@o2.pl>
2018-10-24 10:18:14 +0200

    Add Comments component

--- learn-redux-with-wesbos/client/components/Single.js
+++ learn-redux-with-wesbos/client/components/Single.js
@@ -6,12 +6,14 @@ import Comments from './Comments';
 const Single = (props) => {
   // we need the index of the post to get us the actual post
   // we have the post code/id in the url so we'll use that
-  const i = props.posts.findIndex((post) => post.code === props.params.postId);
+  const { postId } = props.params;
+  const i = props.posts.findIndex((post) => post.code === postId);
   const post = props.posts[i];
+  const postComments = props.comments[postId] || [];
   return (
     <div className="single-photo">
       <Photo i={i} post={post} {...props} />
-      <Comments />
+      <Comments postComments={postComments} />
     </div>
   );
 };
