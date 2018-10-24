54250561b7886529389dd61aefa04a4802eea416
Kamila Matla-Tomczyk <ka1130@o2.pl>
2018-10-24 10:37:56 +0200

    Add the possibility of submitting comments

--- learn-redux-with-wesbos/client/reducers/comments.js
+++ learn-redux-with-wesbos/client/reducers/comments.js
@@ -1,4 +1,29 @@
+function postComments(state=[], action) {
+  switch (action.type) {
+    case 'ADD_COMMENT':
+      // return a copy of the existing state + the new comment
+      return [...state, {
+        user: action.author,
+        text: action.comment
+      }];
+    case 'REMOVE_COMMENT':
+      return state;
+    default:
+      return state;
+  }
+}
+
+// Reducer composition pattern => see the documentation
 function comments(state=[], action) {
+  if(typeof action.postId !== 'undefined') {
+    return {
+      // take the current state
+      ...state,
+      // overwrite this post with a new one
+      // key of the object will be a variable
+      [action.postId]: postComments(state[action.postId], action)
+    }
+  }
   return state;
 }
 
