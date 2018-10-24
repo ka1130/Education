48adc20a29004988b707eff590849efbc855dfb6
Kamila Matla-Tomczyk <ka1130@o2.pl>
2018-10-24 09:54:49 +0200

    Display Single element with the Photo

--- learn-redux-with-wesbos/client/components/Single.js
+++ learn-redux-with-wesbos/client/components/Single.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import Photo from './Photo';
 
-const Single = () => {
+// import comments
+
+const Single = props => {
+  // we need the index of the post to get us the actual post
+  // we have the post code/id in the url so we'll use that
+  const i = props.posts.findIndex(post => post.code === props.params.postId);
+  const post = props.posts[i];
+  console.log(i, post);
   return (
     <div className="single-photo">
-      I'm the single.
+      <Photo i={i} post={post} {...props} />
     </div>
   );
 };
