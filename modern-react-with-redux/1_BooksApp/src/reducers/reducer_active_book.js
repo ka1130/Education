7b067604cf93f2b2c048ee72d73d08b8c84ad209
Kamila Matla-Tomczyk <ka1130@o2.pl>
2018-10-25 17:49:27 +0200

    Remove unnecessary folder

--- modern-react-with-redux/1_BooksApp/src/reducers/reducer_active_book.js
+++
@@ -1,9 +0,0 @@
-// State argument is not application state
-// Only the state this reducer is responsibe for
-export default function(state = null, action) {
-  switch(action.type) {
-    case 'BOOK_SELECTED':
-    return action.payload;
-  }
-  return state;
-}
