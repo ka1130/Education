75b03f4a0ffb35cd751475aebfcc7c41c84f6dbe
Kamila Matla-Tomczyk <ka1130@o2.pl>
2018-10-25 17:45:10 +0200

    Add secret sentry key

--- learn-redux-with-wesbos/client/data/config.js
+++ learn-redux-with-wesbos/client/data/config.js
@@ -1,6 +1,7 @@
 import Raven from 'raven-js';
 
-const sentry_key = 'cb55d4f05cd443ce82303222f77ef5e0';
+const sentry_key = '5eceb800f01841479baff13d0036b3048cd8349ee4144f058f7f9a35ec06b1d0';
+const secret_key = 'af2636e14f1c474da1616cfabbf75a0c42e7732992754bc489bf731f607798c0';
 const sentry_app = '61499';
 export const sentry_url = `https://${sentry_key}@app.getsentry.com/${sentry_app}`;
 
