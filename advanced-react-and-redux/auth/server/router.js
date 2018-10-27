446d9d8544c1072da997d15efcd951f116b1497c
Kamila Matla-Tomczyk <ka1130@o2.pl>
2018-10-27 14:24:51 +0200

    Add authentication controller

--- advanced-react-and-redux/auth/server/router.js
+++ advanced-react-and-redux/auth/server/router.js
@@ -1,6 +1,10 @@
+const Authentication = require('./controllers/authentication');
+
 module.exports = function(app) {
   // get because we're expecting a get request; when user visits this route, '/', then run this fn
-  app.get('/', function(request, response, next) {
-    response.send(['waterbottle', 'phone', 'paper']);
-  });
+  // app.get('/', function(request, response, next) {
+  //   response.send(['waterbottle', 'phone', 'paper']);
+  // });
+
+  app.post('/signup', Authentication.signup);
 }
