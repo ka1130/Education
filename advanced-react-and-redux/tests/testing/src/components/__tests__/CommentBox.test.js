6cfe39b7e4f4887f7d94c9b4d22e90fdc7613af5
Kamila Matla-Tomczyk <ka1130@o2.pl>
2018-10-28 12:05:17 +0100

    Clean up mounted wrapper after running the tests

--- advanced-react-and-redux/tests/testing/src/components/__tests__/CommentBox.test.js
+++ advanced-react-and-redux/tests/testing/src/components/__tests__/CommentBox.test.js
@@ -4,8 +4,15 @@ import CommentBox from 'components/CommentBox';
 
 let wrapper;
 
-it('has a textarea and a button', () => {
+beforeEach(() => {
   wrapper = mount(<CommentBox />);
+});
+
+afterEach(() => {
+  wrapper.unmount();
+});
+
+it('has a textarea and a button', () => {
   expect(wrapper.find('textarea').length).toEqual(1);
   expect(wrapper.find('button').length).toEqual(1);
 });
