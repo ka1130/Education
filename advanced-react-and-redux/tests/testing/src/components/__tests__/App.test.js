89d437764e82e494507214d8e13b288be07039fe
Kamila Matla-Tomczyk <ka1130@o2.pl>
2018-10-28 11:39:36 +0100

    Add absolute paths

--- advanced-react-and-redux/tests/testing/src/components/__tests__/App.test.js
+++ advanced-react-and-redux/tests/testing/src/components/__tests__/App.test.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import App from '../App';
-import CommentBox from '../CommentBox';
+import App from 'components/App';
+import CommentBox from 'components/CommentBox';
+import CommentList from 'components/CommentList';
 
 it('shows a comment box', () => {
   const wrapper = shallow(<App />);
   expect(wrapper.find(CommentBox).length).toEqual(1);
 });
+
+it('shows a comment list', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.find(CommentList).length).toEqual(1);
+})
