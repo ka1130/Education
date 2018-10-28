882f609ac512ffea4ed5668f64a146bf9a18a469
Kamila Matla-Tomczyk <ka1130@o2.pl>
2018-10-28 12:25:42 +0100

    Simulate a change event on textarea, feed it with mock event object, force update the component after the mock function was called

--- advanced-react-and-redux/tests/testing/src/components/__tests__/CommentBox.test.js
+++ advanced-react-and-redux/tests/testing/src/components/__tests__/CommentBox.test.js
@@ -16,3 +16,22 @@ it('has a textarea and a button', () => {
   expect(wrapper.find('textarea').length).toEqual(1);
   expect(wrapper.find('button').length).toEqual(1);
 });
+
+it('has a textarea that users can type in', () => {
+  /* 
+    this second argument to the 'simulate' fn, the 'mock', is an object that gets merged
+    into the real event object that gets passed to our event handler
+    so i gets merged into the event object
+  */
+  wrapper.find('textarea').simulate('change', {
+    target: { value: 'new comment' }
+  });
+  /*
+    now we need to rerender the component, the same way as it happens when we call setState()
+    but when we're calling setState, the component rerenders asynchronously
+    so when setState is called within the handleChange() fn it does not immediately rerender the component
+    but in this test we want to immediately see the change that was made after the setState() call
+    so we need to force the component's update
+  */
+ wrapper.update();
+});
