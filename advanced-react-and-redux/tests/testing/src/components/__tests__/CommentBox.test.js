886d2a202b0720e011f0f79896d3e7b0e29e61e2
Kamila Matla-Tomczyk <ka1130@o2.pl>
2018-10-28 12:45:20 +0100

    Add describe block to avoid duplicate test statements

--- advanced-react-and-redux/tests/testing/src/components/__tests__/CommentBox.test.js
+++ advanced-react-and-redux/tests/testing/src/components/__tests__/CommentBox.test.js
@@ -17,34 +17,34 @@ it('has a textarea and a button', () => {
   expect(wrapper.find('button').length).toEqual(1);
 });
 
-it('has a textarea that users can type in', () => {
-  /* 
-    this second argument to the 'simulate' fn, the 'mock', is an object that gets merged
-    into the real event object that gets passed to our event handler
-    so i gets merged into the event object
-  */
-  wrapper.find('textarea').simulate('change', {
-    target: { value: 'new comment' }
+describe('textarea', () => {
+  beforeEach(() => {
+      /* 
+        this second argument to the 'simulate' fn, the 'mock', is an object that gets merged
+        into the real event object that gets passed to our event handler
+        so i gets merged into the event object
+      */
+    wrapper.find('textarea').simulate('change', {
+      target: { value: 'new comment' }
+    });
+    /*
+      now we need to rerender the component, the same way as it happens when we call setState()
+      but when we're calling setState, the component rerenders asynchronously
+      so when setState is called within the handleChange() fn it does not immediately rerender the component
+      but in this test we want to immediately see the change that was made after the setState() call
+      so we need to force the component's update
+    */
+    wrapper.update();
   });
-  /*
-    now we need to rerender the component, the same way as it happens when we call setState()
-    but when we're calling setState, the component rerenders asynchronously
-    so when setState is called within the handleChange() fn it does not immediately rerender the component
-    but in this test we want to immediately see the change that was made after the setState() call
-    so we need to force the component's update
-  */
- wrapper.update();
 
- // check that the textarea receives the correct value prop => value prop rather than simply value on textarea
-  expect(wrapper.find('textarea').prop('value')).toEqual('new comment');
-});
-
-it('clears textarea after form submission', () => {
-  wrapper.find('textarea').simulate('change', {
-    target: { value: 'new comment' }
+  it('has a textarea that users can type in', () => {
+   // check that the textarea receives the correct value prop => value prop rather than simply value on textarea
+    expect(wrapper.find('textarea').prop('value')).toEqual('new comment');
   });
-  wrapper.update();
-  wrapper.find('form').simulate('submit');
-  wrapper.update();
-  expect(wrapper.find('textarea').prop('value')).toEqual('');
+  
+  it('clears textarea after form submission', () => {
+    wrapper.find('form').simulate('submit');
+    wrapper.update();
+    expect(wrapper.find('textarea').prop('value')).toEqual('');
+  });
 });
