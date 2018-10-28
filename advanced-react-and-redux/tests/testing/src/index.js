83af4727e8d4201e9e610180dfde934ca6d65b47
Kamila Matla-Tomczyk <ka1130@o2.pl>
2018-10-28 14:26:37 +0100

    Add basic reducers

--- advanced-react-and-redux/tests/testing/src/index.js
+++ advanced-react-and-redux/tests/testing/src/index.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from 'reducers';
 
 import App from 'components/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  // the second arg to the createStore fn is the initial state
+  <Provider store={createStore(reducers, {})}> 
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
