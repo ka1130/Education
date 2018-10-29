8968167263afc6918db0c180bfcbffc62ab74e61
Kamila Matla-Tomczyk <ka1130@o2.pl>
2018-10-29 21:49:12 +0100

    Add integration test with moxios

--- advanced-react-and-redux/tests/testing/src/components/CommentBox.js
+++ advanced-react-and-redux/tests/testing/src/components/CommentBox.js
@@ -25,7 +25,7 @@ class CommentBox extends Component {
             <button type="submit">Submit Comment</button>
           </div>
         </form>
-        <button onClick={this.props.fetchComments}>Fetch comments</button>
+        <button className="fetch-comments" onClick={this.props.fetchComments}>Fetch comments</button>
       </>
     );
   }
