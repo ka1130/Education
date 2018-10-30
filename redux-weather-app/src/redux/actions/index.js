186cd3982ae190c01d6719631ed2ebb7c6555c67
Kamila Matla-Tomczyk <ka1130@o2.pl>
2018-10-30 17:22:35 +0100

    Setup basic structure for Redux

---
+++ redux-weather-app/src/redux/actions/index.js
@@ -0,0 +1,6 @@
+import axios from 'axios';
+
+const API_KEY = '1d1ed4c5c279b3103998e234955244b6';
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+const sammple_url = 'https://api.openweathermap.org/data/2.5/forecast?appid=02230cf0e31741f5ca1a2b8f7d73c966&q=lublin,pl';
