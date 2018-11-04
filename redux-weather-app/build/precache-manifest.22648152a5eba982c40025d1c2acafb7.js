88e21dc238be98b4fc90fd49e1a794533935b97d
Kamila Matla-Tomczyk <ka1130@o2.pl>
2018-11-04 23:18:48 +0100

    Add weather-app after a critical git problem

---
+++ redux-weather-app/build/precache-manifest.22648152a5eba982c40025d1c2acafb7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fcfc2d7d891c14615436",
+    "url": "/redux-weather-app/static/js/runtime~main.fcfc2d7d.js"
+  },
+  {
+    "revision": "7ec042ecab0709aff0e4",
+    "url": "/redux-weather-app/static/js/main.7ec042ec.chunk.js"
+  },
+  {
+    "revision": "5a03dff020ed338227a5",
+    "url": "/redux-weather-app/static/js/1.5a03dff0.chunk.js"
+  },
+  {
+    "revision": "7ec042ecab0709aff0e4",
+    "url": "/redux-weather-app/static/css/main.4094b500.chunk.css"
+  },
+  {
+    "revision": "5a03dff020ed338227a5",
+    "url": "/redux-weather-app/static/css/1.577458fd.chunk.css"
+  },
+  {
+    "revision": "d267ad7e20974d366391dae4c67e11a4",
+    "url": "/redux-weather-app/index.html"
+  }
+];
