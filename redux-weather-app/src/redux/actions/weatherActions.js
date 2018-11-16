2833fa7c3cdf2ad206da381af54ea4ea6c85b1e5
Kamila Matla-Tomczyk <ka1130@o2.pl>
2018-11-16 21:48:46 +0100

    Rewrite fetching functions using sagas, delete exemplary files, pass region as parameter to saga

--- redux-weather-app/src/redux/actions/weatherActions.js
+++ redux-weather-app/src/redux/actions/weatherActions.js
@@ -1,39 +1,15 @@
-import axios from 'axios';
 import { 
   FETCH_WEATHER_BEGIN,
   FETCH_WEATHER_SUCCESS,
   FETCH_WEATHER_FAILURE,
-  EXAMPLE_INITIATE,
-  FETCH_WEATHER_INITIATE
 } from 'redux/actions/types';
 
-// rename .env.example with .env and fill in tke keys in order to use them
-// const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
-
 export function fetchWeather(region) {
-  // const { latitude, longitude } = region || {};
-
-  // const getDataByCity = `https://api.openweathermap.org/data/2.5/forecast?q=${region}&appid=${API_KEY}`;
-  // const getDataByCoords = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`;
-
-  // let location = typeof(region) === "object" ? getDataByCoords : getDataByCity;
-
-  // return dispatch => {
-  //   axios.get(location)
-  //   .then(response => {
-  //     dispatch(fetchWeatherSuccess(response));
-  //     return response; 
-  //   })
-  //   .catch(error => dispatch(fetchWeatherFailure(error)));
-  // };
   return {
-    type: FETCH_WEATHER_INITIATE
+    type: FETCH_WEATHER_BEGIN,
+    payload: region
   }
 }
-
-export const fetchWeatherBegin = () => ({
-  type: FETCH_WEATHER_BEGIN
-});
 
 export const fetchWeatherSuccess = response => ({
   type: FETCH_WEATHER_SUCCESS,
--- redux-weather-app/src/redux/sagas/index.js
+++ redux-weather-app/src/redux/sagas/index.js
@@ -1,13 +1,8 @@
 import { takeEvery } from 'redux-saga/effects';
 
 import * as actionTypes from 'redux/actions/types';
-import { exampleSaga } from 'redux/sagas/example';
 import { fetchWeatherSaga } from 'redux/sagas/fetchWeather';
 
-export function* watchExample() {
-  yield takeEvery(actionTypes.EXAMPLE_INITIATE, exampleSaga)
-}
-
 export function* fetchWeather() {
-  yield takeEvery(actionTypes.FETCH_WEATHER_INITIATE, fetchWeatherSaga);
+  yield takeEvery(actionTypes.FETCH_WEATHER_BEGIN, fetchWeatherSaga);
 }
