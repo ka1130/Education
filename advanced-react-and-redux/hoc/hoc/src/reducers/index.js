48e6f2c6a78e68f6369ec605082c8b09be797f50
Kamila Matla-Tomczyk <ka1130@o2.pl>
2019-02-01 12:30:14 +0100

    Setup the app's base

---
+++ advanced-react-and-redux/hoc/hoc/src/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux'
+import commentsReducer from 'reducers/comments'
+
+const rootReducer = combineReducers({ comments: commentsReducer })
+
+export default rootReducer
