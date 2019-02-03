94926e343377da25a4c42c49ead6ae5c9e133f2c
Kamila Matla-Tomczyk <ka1130@o2.pl>
2019-02-03 22:49:25 +0100

    Wire up REST actions

--- modern-react-redux/streams/client/src/components/streams/StreamCreate.js
+++ modern-react-redux/streams/client/src/components/streams/StreamCreate.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { connect } from 'react-redux';
+
+import { createStream } from 'actions';
 
 class StreamCreate extends Component {
   renderError({ error, touched }) {
@@ -23,8 +26,9 @@ class StreamCreate extends Component {
     );
   }
 
-  onSubmit(formValues) {
+  onSubmit = formValues => {
     console.log(formValues);
+    this.props.createStream(formValues);
   }
 
   render() {
@@ -54,7 +58,9 @@ const validate = values => {
   return errors;
 }
 
-export default reduxForm({
+const formWrapper = reduxForm({
   form: 'StreamCreate',
   validate,
-})(StreamCreate);
+})(StreamCreate)
+
+export default connect(null, { createStream })(formWrapper);
