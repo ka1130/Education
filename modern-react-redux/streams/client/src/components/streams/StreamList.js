90867b8497451cc163ae6f8c413ff33cf44a0ae3
Kamila Matla-Tomczyk <ka1130@o2.pl>
2019-02-04 00:28:37 +0100

    Fetch streams from db.json, with some 304 status code issue

--- modern-react-redux/streams/client/src/components/streams/StreamList.js
+++ modern-react-redux/streams/client/src/components/streams/StreamList.js
@@ -1,7 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-const StreamList = () => {
-  return <div>StreamList</div>;
-};
+import { fetchStreams } from 'actions';
 
-export default StreamList;
+class StreamList extends Component {
+  componentDidMount() {
+    this.props.fetchStreams();
+  }
+
+  renderStreams() {
+    this.props.streams.map(stream => console.log(stream));
+    return this.props.streams.map(stream => {
+      return (
+        <div className="item" key={stream.id}>
+          <i className="large middle aligned icon camera" />
+          <div className="content">
+            {stream.title}
+            <div className="description">{stream.description}</div>
+          </div>
+        </div>  
+      );
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Streams</h2>
+        <div className="ui celled list">{this.renderStreams()}</div>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({ streams: Object.values(state.streams) });
+
+export default connect(mapStateToProps, { fetchStreams })(StreamList);
