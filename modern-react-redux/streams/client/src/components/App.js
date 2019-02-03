510e4740d35b38fa1dc1981a5a89667474778c6f
Kamila Matla-Tomczyk <ka1130@o2.pl>
2019-02-03 11:55:47 +0100

    Wire up redux-form

--- modern-react-redux/streams/client/src/components/App.js
+++ modern-react-redux/streams/client/src/components/App.js
@@ -15,7 +15,7 @@ const App = () => {
         <>
           <Header />
           <Route exact path="/" component={StreamList} />
-          <Route path="/streams/new" component={StreamCreate} />
+          <Route path="/streams/new" render={() => <StreamCreate />} />
           <Route path="/streams/edit" component={StreamEdit} />
           <Route path="/streams/delete" component={StreamDelete} />
           <Route path="/streams/show" component={StreamShow} />
--- modern-react-redux/streams/client/src/components/streams/StreamCreate.js
+++ modern-react-redux/streams/client/src/components/streams/StreamCreate.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
-const StreamCreate = () => {
-  return <div>StreamCreate</div>;
-};
+class StreamCreate extends Component {
+  renderInput({ input }) {
+    return <input {...input} />;
+  }
 
-export default StreamCreate;
+  render() {
+    return (
+      <form>
+        <Field name="title" component={this.renderInput} />
+        {/* name: required; a name of a property that this Field is going to manage */}
+        {/* component: either a React component or a function for the Field to call */}
+        <Field name="description" component={this.renderInput} />
+      </form>
+    );
+  }
+}
+
+export default reduxForm({
+  form: 'streamCreate',
+})(StreamCreate);
