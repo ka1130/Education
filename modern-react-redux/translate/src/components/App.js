1dcb1e864fc9580e4c46560ab320243c9def52b6
Kamila Matla-Tomczyk <ka1130@o2.pl>
2019-02-04 19:08:17 +0100

    Add semantic ui

--- modern-react-redux/translate/src/components/App.js
+++ modern-react-redux/translate/src/components/App.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const App = () => {
-  return <h1>App</h1>;
+class App extends Component {
+  render() {
+    return <div className="ui container">App</div>;
+  }
 }
 
 export default App;
