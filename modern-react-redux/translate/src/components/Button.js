7a5e56c39e737169e1d41c31610e617b6b9f0f41
Kamila Matla-Tomczyk <ka1130@o2.pl>
2019-02-04 21:53:50 +0100

    Pass context down to the Button component

--- modern-react-redux/translate/src/components/Button.js
+++ modern-react-redux/translate/src/components/Button.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
+import LanguageContext from 'contexts/LanguageContext';
 
 class Button extends Component {
+  static contextType = LanguageContext;
+  // the above has to be named contextType
+
   render() {
+    console.log(this.context);
     return <button className="ui primary button">Submit</button>;
   }
 }
---
+++ modern-react-redux/translate/src/contexts/LanguageContext.js
@@ -0,0 +1,4 @@
+import React from 'react';
+
+export default React.createContext('english');
+// the above creates a defaultValue, it can be of any type
