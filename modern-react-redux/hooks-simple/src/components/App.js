ea33baed4434e35d5de2bd39e2064c06a828a5bf
Kamila Matla-Tomczyk <ka1130@o2.pl>
2019-02-05 23:51:09 +0100

    Add ResourceList component

--- modern-react-redux/hooks-simple/src/components/App.js
+++ modern-react-redux/hooks-simple/src/components/App.js
@@ -1,14 +1,16 @@
 import React, { useState } from 'react';
+import ResourceList from 'components/ResourceList';
 
 const App = () => {
   const [resource, setResource] = useState('posts');
+  console.log(useState({ foo: 'bar' }));
   return (
     <div>
       <div>
         <button onClick={() => setResource('posts')}>Posts</button>
         <button onClick={() => setResource('todos')}>Todos</button>
       </div>
-      {resource}
+      <ResourceList resource={resource} />
     </div>
   );
 }
