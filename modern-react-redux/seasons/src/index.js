6642723343e6259d1160e2b1356f1c09a4b96a73
Kamila Matla-Tomczyk <ka1130@o2.pl>
2019-02-06 13:24:40 +0100

    Move the fetching function to useEffect()

--- modern-react-redux/seasons/src/index.js
+++ modern-react-redux/seasons/src/index.js
@@ -7,18 +7,15 @@ const App = () => {
   const [lat, setLat] = useState(null);
   const [err, setErr] = useState('');
 
-  const fetchLocation = async () => {
-    const response = await window.navigator.geolocation.getCurrentPosition(
+  useEffect(() => {
+    window.navigator.geolocation.getCurrentPosition(
       pos => setLat(pos.coords.latitude),
       err => setErr(err.message)
     );
 
-    return response;
-  }
-
-  useEffect(() => {
-    fetchLocation();
-  }, [lat, err]);
+  }, []);
+  // ^ We didn'd have componentDidUpdate method so we pass an [] 
+  // because we only want to call useEffect one time, upon rendering
 
   const renderContent = () => {
     if (err && !lat) return <div>Error: {err}</div>;
