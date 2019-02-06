2dd2dc42f38428aafe700d852eecb182a3e4a7d6
Kamila Matla-Tomczyk <ka1130@o2.pl>
2019-02-06 11:51:02 +0100

    Refactor useEffect inner fn IIFE

--- modern-react-redux/hooks-simple/src/components/ResourceList.js
+++ modern-react-redux/hooks-simple/src/components/ResourceList.js
@@ -9,9 +9,15 @@ const ResourceList = ({ resource }) => {
     setResources(response.data);
   }
 
-  useEffect(() => {
-    fetchResource(resource);
-  }, [resource])
+  useEffect(
+    () => {
+      (async resource => {
+        const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`);
+        setResources(response.data);
+      })(resource);
+    }, 
+    [resource]
+  );
 
   console.log(resource)
   console.log(resources)
