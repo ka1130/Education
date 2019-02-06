0c2208ab7c89197b2a87fce8dd9f4b95681d85da
Kamila Matla-Tomczyk <ka1130@o2.pl>
2019-02-06 17:42:49 +0100

    Conditionally render winter/summer info

--- modern-react-redux/seasons-simple/src/App.js
+++ modern-react-redux/seasons-simple/src/App.js
@@ -2,11 +2,9 @@ import React from 'react';
 import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+  state = { lat: null, err: '' }
 
-    this.state = { lat: null, err: '' }
-
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
       position => this.setState({ lat: position.coords.latitude }),
       err => this.setState({ err: err.message })
@@ -17,7 +15,7 @@ class App extends React.Component {
     const { lat, err } = this.state;
 
     if (err && !lat) return <div>Error: {err}</div>;
-    if (!err && lat) return <div>Latitude: {lat}</div>;
+    if (!err && lat) return <SeasonDisplay lat={lat} />;
     
     return <div>Loading</div>;
   }
