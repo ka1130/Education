c6ee5950afd0b83da082340724a6054efc35e998
Kamila Matla-Tomczyk <ka1130@o2.pl>
2019-02-06 12:01:01 +0100

    Make ResourceList a reusable component

--- modern-react-redux/hooks-simple/src/components/ResourceList.js
+++ modern-react-redux/hooks-simple/src/components/ResourceList.js
@@ -1,13 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const ResourceList = ({ resource }) => {
+const useResources = (resource) => {
   const [ resources, setResources ] = useState([]);
-
-  // const fetchResource = async resource => {
-  //   const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`);
-  //   setResources(response.data);
-  // }
 
   useEffect(
     () => {
@@ -19,8 +14,11 @@ const ResourceList = ({ resource }) => {
     [resource]
   );
 
-  console.log(resource)
-  console.log(resources, typeof(resources))
+  return resources;
+}
+
+const ResourceList = ({ resource }) => {
+  const resources = useResources(resource);
 
   return (
     <ul>
