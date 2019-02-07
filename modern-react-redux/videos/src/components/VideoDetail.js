ab2e52f82e8e0d089e6e72d133329311e2efaed9
Kamila Matla-Tomczyk <ka1130@o2.pl>
2019-02-07 16:31:43 +0100

    Rewrite VideoDetail component to a functional component using hooks

--- modern-react-redux/videos/src/components/VideoDetail.js
+++ modern-react-redux/videos/src/components/VideoDetail.js
@@ -1,37 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import YouTube from "react-youtube";
 import "./VideoDetail.css";
 
-class VideoDetail extends React.Component {
-  state = { selectedVideo: null };
+const VideoDetail = props => {
+  const [selectedVideo, setSelectedVideo] = useState(props.selectedVideo);
 
-  componentDidUpdate(prevProps) {
-    if (prevProps.selectedVideo !== this.props.selectedVideo) {
-      this.setState({ selectedVideo: this.props.selectedVideo });
+  useEffect(() => {
+    setSelectedVideo(props.selectedVideo);
+  });
+
+  const opts = {
+    origin: "https://localhost:3000",
+    playerVars: {
+      autoplay: 0
     }
+  };
+
+  let videoId;
+  if (selectedVideo) {
+    videoId = selectedVideo;
+  } else {
+    videoId = "";
   }
 
-  render() {
-    const opts = {
-      origin: "https://localhost:3000",
-      playerVars: {
-        autoplay: 0
-      }
-    };
-
-    let videoId;
-    if (this.props.selectedVideo) {
-      videoId = this.props.selectedVideo;
-    } else {
-      videoId = "";
-    }
-
-    return (
-      <div className="video-detail">
-        <YouTube videoId={videoId} opts={opts} />
-      </div>
-    );
-  }
-}
+  return (
+    <div className="video-detail">
+      <YouTube videoId={videoId} opts={opts} />
+    </div>
+  );
+};
 
 export default VideoDetail;
