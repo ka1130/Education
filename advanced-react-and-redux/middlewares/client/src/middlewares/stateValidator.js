f3ee58bf3923275c0b0a604dbde999ca059cdcc0
Kamila Matla-Tomczyk <ka1130@o2.pl>
2019-02-09 00:30:37 +0100

    Add warning if invalid state schema was detected

--- advanced-react-and-redux/middlewares/client/src/middlewares/stateValidator.js
+++ advanced-react-and-redux/middlewares/client/src/middlewares/stateValidator.js
@@ -2,7 +2,8 @@ import tv4 from "tv4";
 import stateSchema from "./stateSchema";
 
 export default ({ dispatch, getState }) => next => action => {
-  console.log(getState());
   next(action);
-  console.log(tv4.validate(getState(), stateSchema));
+  if (!tv4.validate(getState(), stateSchema)) {
+    alert("Invalid state schema detected");
+  }
 };
