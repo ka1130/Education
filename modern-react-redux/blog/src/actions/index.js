3fc19972514630cc8b5b6fb745d0d6c063c50c95
Kamila Matla-Tomczyk <ka1130@o2.pl>
2019-02-08 15:42:30 +0100

    Setup posts app

---
+++ modern-react-redux/blog/src/actions/index.js
@@ -0,0 +1,16 @@
+import jsonPlaceholder from "apis/jsonPlaceholder";
+
+// export const fetchPosts = () => {
+//   return async (dispatch, getState) => {
+//     const response = await jsonPlaceholder.get("posts");
+//     console.log(getState()); // have a look at the whole store if you want
+//     dispatch({ type: "FETCH_POSTS", payload: response.data });
+//   };
+// };
+// LONGER AND MORE EXPLICIT VERSION ABOVE
+// BELOW: define a fn that return a fn (the same thing)
+
+export const fetchPosts = () => async dispatch => {
+  const response = await jsonPlaceholder.get("posts");
+  dispatch({ type: "FETCH_POSTS", payload: response.data });
+};
