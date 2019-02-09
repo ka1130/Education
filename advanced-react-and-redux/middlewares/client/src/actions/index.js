fc42144e3cd5ba0ef113a4f05af8e301aab58844
Kamila Matla-Tomczyk <ka1130@o2.pl>
2019-02-09 13:15:56 +0100

    Add edit and delete actions

--- advanced-react-and-redux/middlewares/client/src/actions/index.js
+++ advanced-react-and-redux/middlewares/client/src/actions/index.js
@@ -1,12 +1,33 @@
 import axios from "axios";
 import history from "../history";
-import { CREATE_COMMENT, FETCH_COMMENTS, CHANGE_AUTH } from "actions/types";
+import {
+  CREATE_COMMENT,
+  EDIT_COMMENT,
+  DELETE_COMMENT,
+  FETCH_COMMENTS,
+  CHANGE_AUTH
+} from "actions/types";
 
 export const createComment = comment => async dispatch => {
   const response = await axios.post("http://localhost:3001/comments", {
     name: comment
   });
   dispatch({ type: CREATE_COMMENT, payload: response.data });
+  history.push("/");
+};
+
+export const editComment = (id, formValues) => async dispatch => {
+  const response = await axios.patch(
+    `http://localhost:3001/comments/${id}`,
+    formValues
+  );
+  dispatch({ type: EDIT_COMMENT, payload: response.data });
+  history.push("/");
+};
+
+export const deleteComment = id => async dispatch => {
+  const response = await axios.delete(`http://localhost:3001/comments/${id}`);
+  dispatch({ type: DELETE_COMMENT, payload: id });
   history.push("/");
 };
 
--- advanced-react-and-redux/middlewares/client/src/actions/types.js
+++ advanced-react-and-redux/middlewares/client/src/actions/types.js
@@ -1,3 +1,5 @@
 export const CREATE_COMMENT = "CREATE_COMMENT";
+export const EDIT_COMMENT = "EDIT_COMMENT";
+export const DELETE_COMMENT = "DELETE_COMMENT";
 export const FETCH_COMMENTS = "FETCH_COMMENTS";
 export const CHANGE_AUTH = "CHANGE_AUTH";
