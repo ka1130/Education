8e6b0f650f5ba7b420ee567fcf39685a4fbaa342
Kamila Matla-Tomczyk <ka1130@o2.pl>
2019-02-09 23:37:11 +0100

    Style the comments list

--- advanced-react-and-redux/middlewares/client/src/components/comments/CommentItem.js
+++ advanced-react-and-redux/middlewares/client/src/components/comments/CommentItem.js
@@ -1,16 +1,41 @@
-import React from "react";
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import faker from "faker";
 
-const CommentItem = ({ comment }) => {
-  return (
-    <div className="item">
-      <img className="ui avatar image" src={faker.image.avatar()} alt="" />
-      <div className="content">
-        <p className="header">{faker.name.findName()}</p>
-        <div className="description">{comment.name}</div>
+class CommentItem extends Component {
+  renderButtons = comment => {
+    if (this.props.auth) {
+      return (
+        <React.Fragment>
+          <Link to={`/comments/edit/${comment.id}`} className="ui mini button">
+            Edit
+          </Link>
+          <Link
+            to={`/comments/delete/${comment.id}`}
+            className="ui mini button"
+          >
+            Delete
+          </Link>
+        </React.Fragment>
+      );
+    }
+  };
+
+  render() {
+    const { comment } = this.props;
+    return (
+      <div className="item">
+        <div className="right floated content">
+          {this.renderButtons(comment)}
+        </div>
+        <img className="ui avatar image" src={faker.image.avatar()} alt="" />
+        <div className="content">
+          <p className="header">{faker.name.findName()}</p>
+          <div className="description">{comment.name}</div>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default CommentItem;
--- advanced-react-and-redux/middlewares/client/src/components/comments/CommentList.js
+++ advanced-react-and-redux/middlewares/client/src/components/comments/CommentList.js
@@ -8,32 +8,11 @@ import CommentItem from "components/comments/CommentItem";
 class CommentList extends Component {
   componentDidMount() {
     this.props.fetchComments();
-    console.log(faker.image.avatar());
   }
-
-  renderButtons = comment => {
-    if (this.props.auth) {
-      return (
-        <React.Fragment>
-          <Link to={`/comments/edit/${comment.id}`} className="">
-            Edit comment
-          </Link>
-          <Link to={`/comments/delete/${comment.id}`} className="">
-            Delete comment
-          </Link>
-        </React.Fragment>
-      );
-    }
-  };
 
   renderComments = () =>
     this.props.comments.map(comment => (
-      <CommentItem comment={comment} key={comment.id} />
-      // <div key={comment.id} className="item">
-      //   <p>{comment.name}</p>
-
-      //   {this.renderButtons(comment)}
-      // </div>
+      <CommentItem comment={comment} key={comment.id} auth={this.props.auth} />
     ));
 
   render() {
