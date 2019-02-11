6d7d31abe469cbd51497a5c91c60ad0540edc1c3
Kamila Matla-Tomczyk <ka1130@o2.pl>
2019-02-11 23:55:44 +0100

    Clean up

--- sagas-introduction/simple-fetch-with-saga/src/actions/index.js
+++ sagas-introduction/simple-fetch-with-saga/src/actions/index.js
@@ -1,14 +1,7 @@
-import { FETCH_USERS_REQUEST, FETCH_USERS_SUCCESS } from "actions/types";
+import { FETCH_USERS_REQUEST } from "actions/types";
 
 export const fetchUsers = () => {
   return {
     type: FETCH_USERS_REQUEST
-  };
-};
-
-export const fetchUsersSuccess = foo => {
-  console.log(foo);
-  return {
-    type: FETCH_USERS_SUCCESS
   };
 };
