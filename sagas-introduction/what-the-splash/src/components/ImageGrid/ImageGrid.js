38d2eab1060eeb13189d94144115cb659f2910ca
Kamila Matla-Tomczyk <ka1130@o2.pl>
2019-02-12 17:03:20 +0100

    Add Button component

--- sagas-introduction/what-the-splash/src/components/ImageGrid/ImageGrid.js
+++ sagas-introduction/what-the-splash/src/components/ImageGrid/ImageGrid.js
@@ -3,8 +3,7 @@ import { loadImages } from "../../actions";
 import { connect } from "react-redux";
 
 import "./styles.css";
-
-const key = "5f96323678d05ff0c4eb264ef184556868e303b32a2db88ecbf15746e6f25e02";
+import Button from "../Button";
 
 class ImageGrid extends Component {
   componentDidMount() {
@@ -12,7 +11,7 @@ class ImageGrid extends Component {
   }
 
   render() {
-    const { images, error } = this.props;
+    const { images, error, isLoading, loadImages } = this.props;
     return (
       <div className="content">
         <section className="grid">
@@ -24,7 +23,13 @@ class ImageGrid extends Component {
               <img src={image.urls.small} alt={image.user.username} />
             </div>
           ))}
-          <button onClick={this.props.loadImages}>Load images</button>
+          {error && <div className="error">{JSON.stringify(error)}</div>}
+          <Button
+            onClick={() => !isLoading && loadImages()}
+            loading={isLoading}
+          >
+            Load More
+          </Button>
         </section>
         {error && <div className="error">{JSON.stringify(error)}</div>}
       </div>
