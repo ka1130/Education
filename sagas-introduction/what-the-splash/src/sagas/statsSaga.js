80a203ce14db93be07a982f664a8f5cd15c361d3
Kamila Matla-Tomczyk <ka1130@o2.pl>
2019-02-12 17:48:12 +0100

    Handle stats

--- sagas-introduction/what-the-splash/src/sagas/statsSaga.js
+++ sagas-introduction/what-the-splash/src/sagas/statsSaga.js
@@ -1,8 +1,22 @@
-import { take, fork } from "redux-saga/effects";
+import { take, fork, put, call } from "redux-saga/effects";
 import { IMAGES } from "../constants";
+import { fetchImageStats } from "../api";
+import { loadImageStats, setImageStats, setImageStatsError } from "../actions";
 
-function* handleStatsRequest(id) {
-  console.log("fetching stats for ", id);
+export function* handleStatsRequest(id) {
+  for (let i = 0; i < 3; i++) {
+    try {
+      yield put(loadImageStats(id));
+      const res = yield call(fetchImageStats, id);
+      yield put(setImageStats(id, res.downloads.total));
+      // image was loaded so we exit the generator
+      return true;
+    } catch (e) {
+      // we just need to retry and dispactch an error
+      // if we tried more than 3 times
+    }
+  }
+  yield put(setImageStatsError(id));
 }
 
 export default function* watchStatsRequest() {
