4ed8c4d4ad4d5462a030847f2740184aff41e761
Kamila Matla-Tomczyk <ka1130@o2.pl>
2019-03-10 16:35:04 +0100

    Add searchbar

--- material-ui/src/components/Header.js
+++ material-ui/src/components/Header.js
@@ -1,23 +1,37 @@
 import React from "react";
-import { AppBar, Button, Grid, IconButton, Toolbar } from "@material-ui/core";
-import MenuIcon from "@material-ui/icons/Menu";
+import {
+  AppBar,
+  Button,
+  Grid,
+  IconButton,
+  InputBase,
+  Toolbar
+} from "@material-ui/core";
+import { Menu as MenuIcon, Search as SearchIcon } from "@material-ui/icons";
+import { OverrideMaterialUICss } from "override-material-ui-css";
 
 const Header = () => {
   return (
-    <AppBar position="static">
-      <Toolbar>
-        <Grid item xs={1}>
-          <IconButton color="inherit">
-            <MenuIcon />
-          </IconButton>
-        </Grid>
-        <Grid item xs={2}>
-          <IconButton color="inherit">
+    <OverrideMaterialUICss>
+      <AppBar position="static">
+        <Toolbar>
+          <Grid item xs={1}>
+            <IconButton color="inherit">
+              <MenuIcon />
+            </IconButton>
+          </Grid>
+          <Grid item xs={2}>
             <Button variant="contained">Teamwork</Button>
-          </IconButton>
-        </Grid>
-      </Toolbar>
-    </AppBar>
+          </Grid>
+          <Grid item xs={4}>
+            <div className="searchbar">
+              <SearchIcon />
+              <InputBase placeholder="Search…" className="searchbar--input" />
+            </div>
+          </Grid>
+        </Toolbar>
+      </AppBar>
+    </OverrideMaterialUICss>
   );
 };
 
