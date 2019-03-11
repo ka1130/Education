2d3b58508e7994185e628e5d479af2d820405078
Kamila Matla-Tomczyk <ka1130@o2.pl>
2019-03-11 22:41:29 +0100

    Add basic webpack config

--- js-infrastructure/webpack_4_practice/js/entry.js
+++ js-infrastructure/webpack_4_practice/js/entry.js
@@ -1,18 +1,34 @@
-import info from './info.json'
+import info from "./info.json";
 
-import $ from 'jquery'
-import 'popper.js'
-import 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import $ from "jquery";
+import "popper.js";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import '../image/webpack-logo.png'
+import "../image/image.jpg";
+import "../image/webpack-logo.png";
+import "../video/video.mp4";
+import "../css/style.css";
 
-import '../css/style.css'
+$("#logo").attr("src", info.logo);
+$("#title").text(info.title);
 
-$('#logo').attr('src',info.logo)
-$('#title').text(info.title)
+$("#img").attr("src", info.image);
+$(".card-title").text(info.city);
+$(".card-text").text(info.description);
 
+function append_video() {
+  var video = "<video id='video' controls='controls' src=" + info.video + "/>";
+  var banner =
+    "<div class='alert alert-primary' role='alert'>Check out the video!</div>";
+  $(banner).appendTo("#video_box");
+  $(video).appendTo("#video_box");
+}
 
-
-
-
+$("#video_btn").click(() => {
+  if ($("#video").length) {
+    // video has been loaded, do nothing now
+  } else {
+    append_video();
+  }
+});
