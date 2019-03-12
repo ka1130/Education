c6b5e952650bb50cc8172e06cca7fdac454ace66
Kamila Matla-Tomczyk <ka1130@o2.pl>
2019-03-12 22:55:18 +0100

    Add exemplary Button

--- storybook/stories/index.js
+++ storybook/stories/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
 import { Button } from "@storybook/react/demo";
+import Hello from "./components/Hello";
 
 storiesOf("Button", module)
   .add("with text", () => <Button>Hello Button</Button>)
@@ -10,4 +11,9 @@ storiesOf("Button", module)
         😀 😎 👍 💯
       </span>
     </Button>
+  ))
+  .add("foobar", () => (
+    <Hello message="FooBar" onClick={() => console.log("ok")}>
+      hello
+    </Hello>
   ));
