fd3d198a011ecd6dfb40d6df4641f76f7e19e5ad
Kamila Matla-Tomczyk <ka1130@o2.pl>
2019-03-16 16:39:57 +0100

    Configure webpack to handle async/await

--- posts_app_with_custom_webpack_config/src/components/PostList.js
+++ posts_app_with_custom_webpack_config/src/components/PostList.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { connect } from "react-redux";
 
-const PostList = () => {
-  return <div className="ui container">PostList</div>;
-};
+import { fetchPosts } from "redux/actions";
 
-export default PostList;
+class PostList extends React.Component {
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
+
+  render() {
+    return <div className="ui container">PostList</div>;
+  }
+}
+
+export default connect(
+  null,
+  { fetchPosts }
+)(PostList);
--- posts_app_with_custom_webpack_config/webpack.config.js
+++ posts_app_with_custom_webpack_config/webpack.config.js
@@ -20,7 +20,13 @@ module.exports = {
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: ["babel-loader"]
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+            plugins: ["@babel/plugin-transform-runtime"]
+          }
+        }
       }
     ]
   },
