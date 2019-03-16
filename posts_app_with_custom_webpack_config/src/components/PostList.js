cec213f8aea74dcacac0097d957e7556f1ba9597
Kamila Matla-Tomczyk <ka1130@o2.pl>
2019-03-16 22:26:12 +0100

    Display posts list

--- posts_app_with_custom_webpack_config/src/components/PostList.js
+++ posts_app_with_custom_webpack_config/src/components/PostList.js
@@ -8,9 +8,22 @@ class PostList extends React.Component {
     this.props.fetchPosts();
   }
 
+  renderList() {
+    return this.props.posts.map(post => (
+      <div className="item" key={post.id}>
+        <i className="large middle aligned icon user" />
+        <div className="content">
+          <div className="ui desrciption">
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+          </div>
+        </div>
+      </div>
+    ));
+  }
+
   render() {
-    console.log(this.props.posts);
-    return <div className="ui container">PostList</div>;
+    return <div className="ui relaxed divided list">{this.renderList()}</div>;
   }
 }
 
