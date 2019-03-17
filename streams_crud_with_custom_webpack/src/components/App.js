cac480f6909b03d46a4954a39363fbaea82091e1
Kamila Matla-Tomczyk <ka1130@o2.pl>
2019-03-17 23:14:48 +0100

    Add Header component

--- streams_crud_with_custom_webpack/src/components/App.js
+++ streams_crud_with_custom_webpack/src/components/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 
+import Header from "components/Header";
 import StreamCreate from "components/streams/StreamCreate";
 import StreamEdit from "components/streams/StreamEdit";
 import StreamDelete from "components/streams/StreamDelete";
@@ -9,13 +10,16 @@ import StreamShow from "components/streams/StreamShow";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Route exact path="/" component={StreamList} />
-      <Route path="/streams/new" component={StreamCreate} />
-      <Route path="/streams/edit" component={StreamEdit} />
-      <Route path="/streams/show" component={StreamShow} />
-      <Route path="/streams/delete" component={StreamDelete} />
-    </BrowserRouter>
+    <div className="ui container">
+      <BrowserRouter>
+        <Header />
+        <Route exact path="/" component={StreamList} />
+        <Route path="/streams/new" component={StreamCreate} />
+        <Route path="/streams/edit" component={StreamEdit} />
+        <Route path="/streams/show" component={StreamShow} />
+        <Route path="/streams/delete" component={StreamDelete} />
+      </BrowserRouter>
+    </div>
   );
 };
 
