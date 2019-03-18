da91827a14f829e490ad4ae3abe1b1f257e8f184
Kamila Matla-Tomczyk <ka1130@o2.pl>
2019-03-18 23:12:21 +0100

    Display validation errors

--- streams_crud_with_custom_webpack/src/components/streams/StreamCreate.js
+++ streams_crud_with_custom_webpack/src/components/streams/StreamCreate.js
@@ -2,17 +2,27 @@ import React from "react";
 import { Field, reduxForm } from "redux-form";
 
 class StreamCreate extends React.Component {
-  renderInput({ input, label, meta }) {
+  renderError = ({ error, touched }) => {
+    if (touched && error) {
+      return (
+        <div className="ui error message">
+          <div className="header">{error}</div>
+        </div>
+      );
+    }
+  };
+
+  renderInput = ({ input, label, meta }) => {
     // label is a custom prop that we gave to the Field element
     // it will show up as prop here
     return (
       <div className="field">
         <label>{label}</label>
-        <input {...input} />
-        <div>{meta.error}</div>
+        <input {...input} autoComplete="off" />
+        {this.renderError(meta)}
       </div>
     );
-  }
+  };
 
   onSubmit(formValues) {
     console.log(formValues);
@@ -22,7 +32,7 @@ class StreamCreate extends React.Component {
     console.log(this.props);
     return (
       <form
-        className="ui form"
+        className="ui form error"
         onSubmit={this.props.handleSubmit(this.onSubmit)}
       >
         <Field name="title" component={this.renderInput} label="Enter Title" />
