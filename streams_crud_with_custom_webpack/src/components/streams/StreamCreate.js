26609c8eee94380b04b253d7a8c8d1130385cdd1
Kamila Matla-Tomczyk <ka1130@o2.pl>
2019-03-18 22:54:18 +0100

    Handle form submission

--- streams_crud_with_custom_webpack/src/components/streams/StreamCreate.js
+++ streams_crud_with_custom_webpack/src/components/streams/StreamCreate.js
@@ -2,25 +2,38 @@ import React from "react";
 import { Field, reduxForm } from "redux-form";
 
 class StreamCreate extends React.Component {
-  renderInput(formProps) {
-    console.log(formProps);
+  renderInput({ input, label }) {
+    // label is a custom prop that we gave to the Field element
+    // it will show up as prop here
     return (
-      <input
-        value={formProps.input.value}
-        onChange={formProps.input.onChange}
-      />
+      <div className="field">
+        <label>{label}</label>
+        <input {...input} />
+      </div>
     );
+  }
+
+  onSubmit(formValues) {
+    console.log(formValues);
   }
 
   render() {
     console.log(this.props);
     return (
-      <form>
-        <Field name="title" component={this.renderInput} />
+      <form
+        className="ui form"
+        onSubmit={this.props.handleSubmit(this.onSubmit)}
+      >
+        <Field name="title" component={this.renderInput} label="Enter Title" />
         {/* name is required, is like a prop we're going to manage */}
         {/*  a React component or a fn for the Field to call - 
           it has to return an element to be shown on the screen  */}
-        <Field name="description" component={this.renderInput} />
+        <Field
+          name="description"
+          component={this.renderInput}
+          label="Enter Description"
+        />
+        <button className="ui button primary">Submit</button>
       </form>
     );
   }
