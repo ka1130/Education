b238685385ed1aca4a10b7b635c25e30047bf5c8
Kamila Matla-Tomczyk <ka1130@o2.pl>
2019-03-18 22:07:07 +0100

    Wire up redux-form

--- streams_crud_with_custom_webpack/src/components/streams/StreamCreate.js
+++ streams_crud_with_custom_webpack/src/components/streams/StreamCreate.js
@@ -1,7 +1,34 @@
 import React from "react";
+import { Field, reduxForm } from "redux-form";
 
-const StreamCreate = () => {
-  return <div>StreamCreate</div>;
-};
+class StreamCreate extends React.Component {
+  renderInput(formProps) {
+    console.log(formProps);
+    return (
+      <input
+        value={formProps.input.value}
+        onChange={formProps.input.onChange}
+      />
+    );
+  }
 
-export default StreamCreate;
+  render() {
+    console.log(this.props);
+    return (
+      <form>
+        <Field name="title" component={this.renderInput} />
+        {/* name is required, is like a prop we're going to manage */}
+        {/*  a React component or a fn for the Field to call - 
+          it has to return an element to be shown on the screen  */}
+        <Field name="description" component={this.renderInput} />
+      </form>
+    );
+  }
+}
+
+export default reduxForm({
+  form: "streamCreate"
+})(StreamCreate);
+// the value is a name that we provide our form with
+// call it whatever the purpose of the form is
+// at this moment our component get a ton of specific props
--- streams_crud_with_custom_webpack/src/redux/reducers/index.js
+++ streams_crud_with_custom_webpack/src/redux/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from "redux";
+import { reducer as formReducer } from "redux-form";
 
 const rootReducer = combineReducers({
-  foo: () => "anything but undefined"
+  form: formReducer
+  // ^ this key has to be called form
 });
 
 export default rootReducer;
