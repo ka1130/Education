719461472b9197824f5be2f85bfac2acf0e87e04
Kamila Matla-Tomczyk <ka1130@o2.pl>
2019-03-19 22:47:01 +0100

    Display fetched streams

--- streams_crud_with_custom_webpack/client/src/components/streams/StreamList.js
+++ streams_crud_with_custom_webpack/client/src/components/streams/StreamList.js
@@ -7,12 +7,28 @@ class StreamList extends React.Component {
     this.props.fetchAllStreams();
   }
 
+  renderList = () =>
+    this.props.streams.map(stream => (
+      <div className="item" key={stream.id}>
+        <i className="large middle aligned icon camera" />
+        <div className="content">
+          {stream.title}
+          <div className="description">{stream.description}</div>
+        </div>
+      </div>
+    ));
+
   render() {
-    return <div>StreamList</div>;
+    return (
+      <div>
+        <h2>Streams</h2>
+        <div className="ui celled list">{this.renderList()}</div>
+      </div>
+    );
   }
 }
 
-const mapStateToProps = state => ({ streams: state.streams });
+const mapStateToProps = state => ({ streams: Object.values(state.streams) });
 
 export default connect(
   mapStateToProps,
