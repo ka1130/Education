f99cb49fc098794f6f6be8021bee544687a62816
Kamila Matla-Tomczyk <ka1130@o2.pl>
2019-03-21 20:50:07 +0100

    Sign in/out with button

--- streams_crud_with_custom_webpack/client/src/components/GoogleAuth.js
+++ streams_crud_with_custom_webpack/client/src/components/GoogleAuth.js
@@ -22,13 +22,31 @@ class GoogleAuth extends React.Component {
     this.setState({ isSignedIn: this.auth.isSignedIn.get() });
   };
 
+  onSignInClick = () => {
+    this.auth.signIn();
+  };
+
+  onSignOutClick = () => {
+    this.auth.signOut();
+  };
+
   renderAuthButton() {
     if (this.state.isSignedIn === null) {
-      return <div>I don't know if you're signed in</div>;
+      return null;
     } else if (this.state.isSignedIn) {
-      return <div>Is signed in</div>;
+      return (
+        <button className="ui red google button" onClick={this.onSignOutClick}>
+          <i className="google icon" />
+          Sign Out
+        </button>
+      );
     } else {
-      return <div>not signed in</div>;
+      return (
+        <button className="ui red google button" onClick={this.onSignInClick}>
+          <i className="google icon" />
+          Sign In with Google
+        </button>
+      );
     }
   }
 
