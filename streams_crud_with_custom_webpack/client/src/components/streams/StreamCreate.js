a68537ccce5d69412a266d39b31e77ff632e94df
Kamila Matla-Tomczyk <ka1130@o2.pl>
2019-03-22 21:29:54 +0100

    Reuse StreamForm

--- streams_crud_with_custom_webpack/client/src/components/streams/StreamCreate.js
+++ streams_crud_with_custom_webpack/client/src/components/streams/StreamCreate.js
@@ -1,32 +1,9 @@
 import React from "react";
 import { connect } from "react-redux";
-import { Field, reduxForm } from "redux-form";
 import { createStream } from "redux/actions";
+import StreamForm from "components/streams/StreamForm";
 
 class StreamCreate extends React.Component {
-  renderError = ({ error, touched }) => {
-    if (touched && error) {
-      return (
-        <div className="ui error message">
-          <div className="header">{error}</div>
-        </div>
-      );
-    }
-  };
-
-  renderInput = ({ input, label, meta }) => {
-    // label is a custom prop that we gave to the Field element
-    // it will show up as prop here
-    const className = `field ${meta.error && meta.touched ? "error" : ""}`;
-    return (
-      <div className={className}>
-        <label>{label}</label>
-        <input {...input} autoComplete="off" />
-        {this.renderError(meta)}
-      </div>
-    );
-  };
-
   onSubmit = formValues => {
     this.props.createStream(formValues);
   };
@@ -34,48 +11,15 @@ class StreamCreate extends React.Component {
   render() {
     console.log(this.props);
     return (
-      <form
-        className="ui form error"
-        onSubmit={this.props.handleSubmit(this.onSubmit)}
-      >
-        <Field name="title" component={this.renderInput} label="Enter Title" />
-        {/* name is required, is like a prop we're going to manage */}
-        {/*  a React component or a fn for the Field to call - 
-          it has to return an element to be shown on the screen  */}
-        <Field
-          name="description"
-          component={this.renderInput}
-          label="Enter Description"
-        />
-        <button className="ui button primary">Submit</button>
-      </form>
+      <div>
+        <h3>Create a Stream</h3>
+        <StreamForm onSubmit={this.onSubmit} />
+      </div>
     );
   }
 }
 
-const validate = formValues => {
-  const errors = {};
-
-  if (!formValues.title) {
-    errors.title = "You must enter a title";
-  }
-
-  if (!formValues.description) {
-    errors.description = "You must enter a description";
-  }
-
-  return errors;
-};
-
-const formWrapper = reduxForm({
-  form: "streamCreate",
-  validate
-})(StreamCreate);
-// the value is a name that we provide our form with
-// call it whatever the purpose of the form is
-// at this moment our component get a ton of specific props
-
 export default connect(
   null,
   { createStream }
-)(formWrapper);
+)(StreamCreate);
--- streams_crud_with_custom_webpack/client/src/components/streams/StreamEdit.js
+++ streams_crud_with_custom_webpack/client/src/components/streams/StreamEdit.js
@@ -1,13 +1,25 @@
 import React from "react";
 import { connect } from "react-redux";
+import { fetchAllStreams } from "redux/actions";
 
-const StreamEdit = props => {
-  console.log(props.stream);
-  return <div>StreamEdit</div>;
-};
+class StreamEdit extends React.Component {
+  componentDidMount() {
+    this.props.fetchAllStreams();
+  }
+  render() {
+    const { stream } = this.props;
+    if (!stream) {
+      return <p>Loading...</p>;
+    }
+    return <div>{stream.title}</div>;
+  }
+}
 
 const mapStateToProps = (state, ownProps) => ({
   stream: state.streams[ownProps.match.params.id]
 });
 
-export default connect(mapStateToProps)(StreamEdit);
+export default connect(
+  mapStateToProps,
+  { fetchAllStreams }
+)(StreamEdit);
