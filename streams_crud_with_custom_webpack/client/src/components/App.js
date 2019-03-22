eaabe29d2051ddd4c43ce797d2c9905ff968f7d7
Kamila Matla-Tomczyk <ka1130@o2.pl>
2019-03-22 18:31:15 +0100

    Make StreamEdit a smart component and pass it stream from router

--- streams_crud_with_custom_webpack/client/src/components/App.js
+++ streams_crud_with_custom_webpack/client/src/components/App.js
@@ -16,7 +16,7 @@ const App = () => {
         <Header />
         <Route exact path="/" component={StreamList} />
         <Route path="/streams/new" component={StreamCreate} />
-        <Route path="/streams/edit" component={StreamEdit} />
+        <Route path="/streams/edit/:id" component={StreamEdit} />
         <Route path="/streams/show" component={StreamShow} />
         <Route path="/streams/delete" component={StreamDelete} />
       </Router>
