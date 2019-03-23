8ad0be3cfa6698cb15922280ec745d3dcb1e6445
Kamila Matla-Tomczyk <ka1130@o2.pl>
2019-03-23 11:37:16 +0100

    Add actions to the modal

--- streams_crud_with_custom_webpack/client/src/components/Modal.js
+++ streams_crud_with_custom_webpack/client/src/components/Modal.js
@@ -1,10 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { history } from "customHistory";
 
 const Modal = () =>
   ReactDOM.createPortal(
-    <div className="ui dimmer modals visible active">
-      <div className="ui standard modal visible active">jjjlkj</div>
+    <div
+      onClick={() => {
+        history.push("/");
+      }}
+      className="ui dimmer modals visible active"
+    >
+      <div
+        onClick={e => e.stopPropagation()}
+        className="ui standard modal visible active"
+      >
+        <div className="header">Delete stream</div>
+        <div className="content">Are you sure?</div>
+        <div className="actions">
+          <button className="ui primary button">Delete</button>
+          <button className="ui button">Cancel</button>
+        </div>
+      </div>
     </div>,
     document.getElementById("modal")
   );
