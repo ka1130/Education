dbb1dfc1a4f1c66dd3bbc3c8be0f937dda972a8e
Kamila Matla-Tomczyk <ka1130@o2.pl>
2019-03-25 22:09:19 +0100

    Initialize giphy project

---
+++ react-geek-udemy/giphy/webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+  entry: "./index.js",
+  output: {
+    path: path.resolve(__dirname),
+    publicPath: "/",
+    filename: "bundle.js"
+  },
+  devtool: "inline-source-map",
+  resolve: {
+    extensions: [".js", ".jsx"],
+    modules: [__dirname, "node_modules/"]
+    // the above is responsible for setting absolute paths
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+            plugins: ["@babel/plugin-transform-runtime"]
+          }
+        }
+      }
+    ]
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()],
+  mode: "development",
+  devServer: {
+    contentBase: "./",
+    hot: true
+  }
+};
