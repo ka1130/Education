4c8154ba2fb9ff4a361a9efc8f6bca4aa403fac9
Kamila Matla-Tomczyk <ka1130@o2.pl>
2019-03-25 22:28:10 +0100

    Configure environment variables

--- react-geek-udemy/giphy/components/App.js
+++ react-geek-udemy/giphy/components/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 const App = () => {
+  console.log(process.env.REACT_APP_API_KEY);
   return <div>App</div>;
 };
 
--- react-geek-udemy/giphy/webpack.config.js
+++ react-geek-udemy/giphy/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const webpack = require("webpack");
+const Dotenv = require("dotenv-webpack");
 
 module.exports = {
   entry: "./index.js",
@@ -29,7 +30,7 @@ module.exports = {
       }
     ]
   },
-  plugins: [new webpack.HotModuleReplacementPlugin()],
+  plugins: [new webpack.HotModuleReplacementPlugin(), new Dotenv()],
   mode: "development",
   devServer: {
     contentBase: "./",
