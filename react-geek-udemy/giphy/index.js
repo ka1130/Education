f8187fa4fa7eeed03c98231a0265d66d57492ec0
Kamila Matla-Tomczyk <ka1130@o2.pl>
2019-03-25 22:20:43 +0100

    Wire up redux

--- react-geek-udemy/giphy/index.js
+++ react-geek-udemy/giphy/index.js
@@ -1,6 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+// import rootReducer from "reducers";
 
 import App from "components/App";
 
-ReactDOM.render(<App />, document.getElementById("app"));
+// const store = createStore(rootReducer);
+
+const store = createStore(() => ({}));
+// reducer -> now we return just empty object which will be our store's state
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("app")
+);
