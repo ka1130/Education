fc38665c88468b923451f97d9633f3c1a6cf785a
Kamila Matla-Tomczyk <ka1130@o2.pl>
2019-03-31 11:17:29 +0200

    Add lightbox functionality to the random image

--- react-geek-udemy/giphy/components/Pages/RandomPage/index.js
+++ react-geek-udemy/giphy/components/Pages/RandomPage/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
 import { getRandomGiphy } from "actions/random";
+import { showGiphy } from "actions/giphy-display";
 import styles from "./RandomPage.css";
 
 class RandomPage extends React.Component {
@@ -9,14 +10,14 @@ class RandomPage extends React.Component {
   }
 
   render() {
-    const { getRandomGiphy, randomGiphy } = this.props;
+    const { getRandomGiphy, randomGiphy, showGiphy } = this.props;
     const imgUrl = randomGiphy ? randomGiphy.thumbnail : "";
     return (
       <div className={styles.container}>
         <button className={styles.button} onClick={getRandomGiphy}>
           Next Giphy
         </button>
-        <img src={imgUrl} />
+        <img src={imgUrl} onClick={() => showGiphy(randomGiphy)} />
       </div>
     );
   }
@@ -26,5 +27,5 @@ const mapStateToProps = state => ({ randomGiphy: state.randomGiphy });
 
 export default connect(
   mapStateToProps,
-  { getRandomGiphy }
+  { getRandomGiphy, showGiphy }
 )(RandomPage);
--- react-geek-udemy/giphy/reducers/random.js
+++ react-geek-udemy/giphy/reducers/random.js
@@ -12,7 +12,6 @@ function transformRandomGiphyResult(rawResult) {
 export default (state = null, action) => {
   switch (action.type) {
     case GET_RANDOM_GIPHY_SUCCESS:
-      console.log("GET_RANDOM_GIPHY_SUCCESS payload is", action.payload);
       return transformRandomGiphyResult(action.payload);
     case LOCATION_CHANGE:
       return null;
