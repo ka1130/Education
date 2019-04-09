8341b5cd9645afd6a3545fb69bc9f643ec3725e2
Kamila Matla-Tomczyk <ka1130@o2.pl>
2019-04-09 22:47:59 +0200

    Add custom hook, change document title accordingly

--- hooks-react-conf-2018/src/Greeting.js
+++ hooks-react-conf-2018/src/Greeting.js
@@ -1,20 +1,31 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
-const Greeting = props => {
-  const [name, setName] = useState("Mary");
+const Greeting = () => {
+  const name = useFormInput("Mary");
+  const surname = useFormInput("Poppins");
+
+  useEffect(() => {
+    document.title = `${name.value} ${surname.value}`;
+  });
 
   return (
     <form className="ui form">
       <div className="field">
         <label>First Name</label>
-        <input type="text" value={name} />
+        <input type="text" {...name} />
       </div>
       <div className="field">
         <label>Last Name</label>
-        <input type="text" />
+        <input type="text" {...surname} />
       </div>
     </form>
   );
+};
+
+const useFormInput = initialValue => {
+  const [value, setValue] = useState(initialValue);
+  const handleValueChange = e => setValue(e.target.value);
+  return { value, onChange: handleValueChange };
 };
 
 export default Greeting;
