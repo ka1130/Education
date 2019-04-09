bb82a0c3c5afc12d407cec2b2db27b2206f58c34
Kamila Matla-Tomczyk <ka1130@o2.pl>
2019-04-09 22:18:38 +0200

    Setup hooks project

---
+++ hooks-react-conf-2018/webpack.config.js
@@ -0,0 +1,32 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/",
+    filename: "bundle.js"
+  },
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+            plugins: ["@babel/plugin-transform-runtime"]
+          }
+        }
+      }
+    ]
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()],
+  devServer: {
+    contentBase: "./dist",
+    hot: true
+  }
+};
