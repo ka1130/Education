3a95bb491d028df70397c62633fa2314b6f5676c
Kamila Matla-Tomczyk <ka1130@o2.pl>
2019-04-10 21:11:02 +0200

    Setup burger project

--- hooks-react-conf-2018/src/Greeting.js
+++ hooks-react-conf-2018/src/Greeting.js
@@ -1,8 +1,10 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
+// import { ThemeContext, LocaleContext } from "./context";
 
 const Greeting = () => {
   const name = useFormInput("Mary");
   const surname = useFormInput("Poppins");
+  // const theme = useContext(ThemeContext);
 
   useEffect(() => {
     document.title = `${name.value} ${surname.value}`;
