ccbce14f46e8085780f28afb6af6def02f840e2b
Kamila Matla-Tomczyk <ka1130@o2.pl>
2019-04-12 19:10:11 +0200

    Dynamically render Burger content

--- burger-project/src/components/Burger/index.js
+++ burger-project/src/components/Burger/index.js
@@ -1,13 +1,21 @@
 import React from "react";
+import uuidv4 from "uuid/v4";
 import BurgerIngredient from "components/Burger/BurgerIngredient";
 import styles from "./Burger.module.css";
 
-const Burger = props => {
+const Burger = ({ ingredients }) => {
+  const renderContent = obj => {
+    const sum = Object.values(obj).reduce((total, amount) => total + amount);
+    if (!sum) return <p>Start adding ingredients</p>;
+    return Object.keys(obj).map(el => (
+      <BurgerIngredient key={uuidv4()} type={el} />
+    ));
+  };
+
   return (
     <div className={styles.burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="meat" />
+      {renderContent(ingredients)}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
--- burger-project/src/containers/BurgerBuilder/index.js
+++ burger-project/src/containers/BurgerBuilder/index.js
@@ -1,10 +1,19 @@
 import React, { useState, useEffect } from "react";
 import Burger from "components/Burger";
 
+const initialIngredients = {
+  salad: 0,
+  bacon: 0,
+  cheese: 0,
+  meat: 0
+};
+
 const BurgerBuilder = props => {
+  const [ingredients, setIngredients] = useState(initialIngredients);
+  console.log(ingredients);
   return (
     <>
-      <Burger />
+      <Burger ingredients={ingredients} />
       <div>Build Controls</div>
     </>
   );
