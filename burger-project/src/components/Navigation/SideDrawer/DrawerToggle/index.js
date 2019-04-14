fa4927712877dcdc0377618505ce0c4bf64ab762
Kamila Matla-Tomczyk <ka1130@o2.pl>
2019-04-14 21:44:33 +0200

    Render hamburger menu

--- burger-project/src/components/Navigation/SideDrawer/DrawerToggle/index.js
+++ burger-project/src/components/Navigation/SideDrawer/DrawerToggle/index.js
@@ -1,7 +1,14 @@
 import React from "react";
+import styles from "./DrawerToggle.module.scss";
 
 const DrawerToggle = ({ onSideDrawerToggle }) => {
-  return <div onClick={onSideDrawerToggle}>MENU</div>;
+  return (
+    <div className={styles.drawerToggle} onClick={onSideDrawerToggle}>
+      <div />
+      <div />
+      <div />
+    </div>
+  );
 };
 
 export default DrawerToggle;
