f480e0218fdd15c54bd4d07bdfdae77076b462f8
Kamila Matla-Tomczyk <ka1130@o2.pl>
2019-05-05 15:41:38 +0200

    Add custom error page

---
+++ next-js/pages/_error.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Link from "next/link";
+
+const Error = () => {
+  return (
+    <div>
+      <h1>Oops, something went wrong</h1>
+      <p>
+        Try{" "}
+        <Link href="/">
+          <a>going back</a>
+        </Link>
+      </p>
+    </div>
+  );
+};
+
+export default Error;
