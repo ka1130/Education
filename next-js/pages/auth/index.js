3c7038149f68d78d8935dcc94587584cba38c187
Kamila Matla-Tomczyk <ka1130@o2.pl>
2019-05-05 16:09:04 +0200

    Initialize props with getInitialProps lifecycle method

--- next-js/pages/auth/index.js
+++ next-js/pages/auth/index.js
@@ -1,13 +1,20 @@
 import React from "react";
 import User from "../../components/User";
 
-const Auth = () => {
+const Auth = ({ gender }) => {
   return (
     <div>
       <h1>The auth page</h1>
       <User name="Mike" age="32" />
+      <p>Gender: {gender}</p>
     </div>
   );
+};
+
+Auth.getInitialProps = context => {
+  return {
+    gender: "man"
+  };
 };
 
 export default Auth;
--- next-js/pages/index.js
+++ next-js/pages/index.js
@@ -2,25 +2,32 @@ import React from "react";
 import Link from "next/link";
 import Router from "next/router";
 
-const Main = () => {
-  return (
-    <div>
-      <h1>The main page</h1>
-      <p>
-        Go to{" "}
-        <Link href="/auth">
-          <a>Auth</a>
-        </Link>
-      </p>
-      <button
-        onClick={() => {
-          Router.push("/auth");
-        }}
-      >
-        Go to Auth
-      </button>
-    </div>
-  );
-};
+class Main extends React.Component {
+  static async getInitialProps(context) {
+    console.log(context);
+    return { appName: "Super App" };
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>The main page {this.props.appName}</h1>
+        <p>
+          Go to{" "}
+          <Link href="/auth">
+            <a>Auth</a>
+          </Link>
+        </p>
+        <button
+          onClick={() => {
+            Router.push("/auth");
+          }}
+        >
+          Go to Auth
+        </button>
+      </div>
+    );
+  }
+}
 
 export default Main;
