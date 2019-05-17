8db6439985298201e80beb1820cdcd7f461511ef
Kamila Matla-Tomczyk <ka1130@o2.pl>
2019-05-17 23:18:29 +0200

    Wire up redux-form

--- advanced-react-and-redux/auth/client/src/components/auth/Signup.js
+++ advanced-react-and-redux/auth/client/src/components/auth/Signup.js
@@ -1,16 +1,29 @@
 import React from "react";
+import { reduxForm, Field } from "redux-form";
 
-const Signup = () => {
+const Signup = ({ handleSubmit }) => {
+  const onSubmit = formProps => {
+    console.log(formProps);
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <fieldset>
         <label>Email</label>
+        <Field name="email" type="text" component="input" autoComplete="none" />
       </fieldset>
       <fieldset>
         <label>Password</label>
+        <Field
+          name="password"
+          type="password"
+          component="input"
+          autoComplete="none"
+        />
       </fieldset>
+      <button>Sign Up!</button>
     </form>
   );
 };
 
-export default Signup;
+export default reduxForm({ form: "signup" })(Signup);
