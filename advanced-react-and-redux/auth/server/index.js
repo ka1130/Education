e5759ee629227e41ddaa766a1bc9a2818ec32229
Kamila Matla-Tomczyk <ka1130@o2.pl>
2019-05-18 11:05:13 +0200

    Configure CORS on the server side

--- advanced-react-and-redux/auth/server/index.js
+++ advanced-react-and-redux/auth/server/index.js
@@ -1,18 +1,20 @@
-const express = require ('express'); 
-const http = require ('http');
+const express = require("express");
+const http = require("http");
 // http is a native node library, working at a very low level with http requests that are coming
-const bodyParser = require ('body-parser');
-const morgan  = require ('morgan');
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
 const app = express(); // an instance of express
-const router = require('./router');
-const mongoose = require('mongoose');
+const router = require("./router");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
 // DB setup - we pass an url here; it makes a new database inside of MongoDB called auth
-mongoose.connect('mongodb://localhost:27017/auth', { useNewUrlParser: true });
+mongoose.connect("mongodb://localhost:27017/auth", { useNewUrlParser: true });
 
 // App setup
-app.use(morgan('combined'));
-app.use(bodyParser.json({ type: '*/*' }));
+app.use(morgan("combined"));
+app.use(cors());
+app.use(bodyParser.json({ type: "*/*" }));
 /* 
   morgan and bodyParser are middlewars, all requests will be passed through them
   morgan is a logging framework - when I refresh the page for instance, I get some logging to the terminal's console
@@ -29,4 +31,4 @@ router(app);
 const port = process.env.PORT || 3090;
 const server = http.createServer(app);
 server.listen(port);
-console.log('Server listening on', port);
+console.log("Server listening on", port);
