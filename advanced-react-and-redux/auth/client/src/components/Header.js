bebb76d184fe411799fa6014df82345d17de9aed
Kamila Matla-Tomczyk <ka1130@o2.pl>
2019-05-18 21:32:17 +0200

    Conditionally render links and style header

--- advanced-react-and-redux/auth/client/src/components/Header.js
+++ advanced-react-and-redux/auth/client/src/components/Header.js
@@ -1,15 +1,37 @@
 import React from "react";
+import { connect } from "react-redux";
 import { Link } from "react-router-dom";
+import "./Header.css";
 
-const Header = () => {
+const Header = ({ authenticated }) => {
+  const renderLinks = () => {
+    if (authenticated) {
+      return (
+        <div>
+          <Link to="/signout">Sign out</Link>
+          <Link to="/feature">Feature</Link>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <Link to="/signin">Sign in</Link>
+          <Link to="/signup">Sign up</Link>
+        </div>
+      );
+    }
+  };
+
   return (
-    <div>
+    <div className="header">
       <Link to="/">Redux Auth</Link>
-      <Link to="/signin">Sign in</Link>
-      <Link to="/signup">Sign up</Link>
-      <Link to="/signout">Sign out</Link>
-      <Link to="/feature">Feature</Link>
+      {renderLinks()}
     </div>
   );
 };
-export default Header;
+
+const mapStateToProps = state => ({
+  authenticated: state.auth.authenticated
+});
+
+export default connect(mapStateToProps)(Header);
