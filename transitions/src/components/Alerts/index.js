bb087f87c145a15f620fc3a48cf95b50d2b4867b
Kamila Matla-Tomczyk <ka1130@o2.pl>
2020-02-02 00:13:25 +0100

    Animate opacity for SimpleAlert

--- transitions/src/components/Alerts/index.js
+++ transitions/src/components/Alerts/index.js
@@ -2,10 +2,11 @@ import React from "react";
 import { connect } from "react-redux";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
 import { dismissAlert } from "redux/actions/alerts";
+import Toast from "react-bootstrap/Toast";
 import transitions from "./transitions.module.scss";
 import styles from "./styles.module.scss";
 
-const Alert = ({ alert, dismissAlert }) => (
+const SimpleAlert = ({ alert, dismissAlert }) => (
   <div
     className={`${styles[alert.csn]} ${styles.alert}`}
     onClick={() => dismissAlert(alert)}
@@ -14,12 +15,30 @@ const Alert = ({ alert, dismissAlert }) => (
   </div>
 );
 
+const ToastAlert = ({ alert, dismissAlert }) => (
+  <Toast
+    className={`${styles[alert.csn]} ${styles.alert}`}
+    onClick={() => dismissAlert(alert)}
+  >
+    <Toast.Header>
+      {/* <img src="holder.js/20x20?text=%20" className="rounded mr-2" alt="" /> */}
+      <strong className="mr-auto"> {alert.type}</strong>
+      {/* <small>11 mins ago</small> */}
+    </Toast.Header>
+    <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
+  </Toast>
+);
+
 const Alerts = ({ alerts, onAlertDismiss }) => (
   <TransitionGroup className={styles.alerts}>
     {alerts.map(alert => (
       <CSSTransition key={alert.id} timeout={500} classNames={transitions}>
         {/* maybe css modules instead of global styles as above? */}
-        <Alert alert={alert} key={alert.id} dismissAlert={onAlertDismiss} />
+        <SimpleAlert
+          alert={alert}
+          key={alert.id}
+          dismissAlert={onAlertDismiss}
+        />
       </CSSTransition>
     ))}
   </TransitionGroup>
